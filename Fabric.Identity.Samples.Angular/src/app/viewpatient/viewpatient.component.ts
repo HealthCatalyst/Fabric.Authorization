88b30dc7f396b057d043c128671c81c608d3dd32
michael.vidal <michael.vidal@healthcatalyst.com>
2017-03-31 10:21:17 -0400

    Update angular app to show a better error message when the sample patient API can't be contacted. Show current profile object of the user even if we don't have permission to call the api.

--- Fabric.Identity.Samples.Angular/src/app/viewpatient/viewpatient.component.ts
+++ Fabric.Identity.Samples.Angular/src/app/viewpatient/viewpatient.component.ts
@@ -1,6 +1,7 @@
 ﻿import { Component } from '@angular/core';
 import { Http, Headers, RequestOptions } from '@angular/http';
 import { AuthService } from '../shared/services/auth.service';
+import { User } from 'oidc-client';
 
 @Component({
   selector: 'app-viewpatient',
@@ -8,20 +9,24 @@ import { AuthService } from '../shared/services/auth.service';
   styleUrls: ['./viewpatient.component.css']
 })
 export class ViewpatientComponent {
-    public patientDetails: PatientDetails
+    public patientDetails: PatientDetails;
+    public errorMessage: string;
+    public authenticatedUser: User;
 
     constructor(http: Http, private authService: AuthService) {
         authService.getUser().then(user => {
+            this.authenticatedUser = user;
             let authHeaders = new Headers();
             authHeaders.append('Authorization', 'Bearer ' + user.access_token);
             authHeaders.append('Content-Type', 'application/json');
 
             let options = new RequestOptions({ headers: authHeaders });
 
-            http.get('http://localhost:5003/patients/123', options).subscribe(result => {
-                this.patientDetails = result.json() as PatientDetails;
-            });
-        })
+            http.get('http://localhost:5003/patients/123', options).subscribe(
+                result => { this.patientDetails = result.json() as PatientDetails; },
+                error => { this.errorMessage = <any> error }
+            );
+        });
     }
 
   
