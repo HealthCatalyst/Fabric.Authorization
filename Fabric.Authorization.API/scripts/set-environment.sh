93a4058008ee7c7879397f4d9482cc458d40a620
michael.vidal <michael.vidal@healthcatalyst.com>
2017-07-10 11:34:43 -0400

    Added an echo to see if the environment variable is being set using the logging task.

--- Fabric.Authorization.API/scripts/set-environment.sh
+++ Fabric.Authorization.API/scripts/set-environment.sh
@@ -4,5 +4,6 @@ IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}
 echo "CouchDb Container running on $IP"
 #CouchDbSettings__Server="http://$IP:5984"
 #export CouchDbSettings__Server
-#echo "CouchDb Server URL: $CouchDbSettings__Server"
 echo "##vso[task.setvariable variable=CouchDbSettings__Server;]http://$IP:5984"
+echo "CouchDb Server URL: $CouchDbSettings__Server"
+
