cefb864e59915030fd379993996a9a12b8ef7af5
michael.vidal <michael.vidal@healthcatalyst.com>
2017-07-12 09:21:01 -0400

    Updated Fabric.Platform.Bootstrappers.Nancy nuget package to fix a bug.

--- Fabric.Authorization.API/scripts/setup-samples.sh
+++ Fabric.Authorization.API/scripts/setup-samples.sh
@@ -10,12 +10,12 @@ echo ""
 
 echo "configuring Fabric.Authorization for samples..."
 echo "setting up clients..."
-curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $accesstoken" -H "fabric-end-user-subject-id: test" -d "{ \"id\": \"fabric-mvcsample\", \"name\": \"Sample Fabric MVC Client\", \"topLevelSecurableItem\": { \"name\":\"fabric-mvcsample\"}}" http://localhost:5004/clients/
-curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $accesstoken" -H "fabric-end-user-subject-id: test" -d "{ \"id\": \"fabric-angularsample\", \"name\": \"Sample Fabric Angular Client\", \"topLevelSecurableItem\": { \"name\":\"fabric-angularsample\"}}" http://localhost:5004/clients/
+curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $accesstoken" -d "{ \"id\": \"fabric-mvcsample\", \"name\": \"Sample Fabric MVC Client\", \"topLevelSecurableItem\": { \"name\":\"fabric-mvcsample\"}}" http://localhost:5004/clients/
+curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $accesstoken" -d "{ \"id\": \"fabric-angularsample\", \"name\": \"Sample Fabric Angular Client\", \"topLevelSecurableItem\": { \"name\":\"fabric-angularsample\"}}" http://localhost:5004/clients/
 echo ""
 
 viewerRole="FABRIC\\\Health Catalyst Viewer"
 editorRole="FABRIC\\\Health Catalyst Editor"
 echo "setting up sample groups..."
-curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $accesstoken" -H "fabric-end-user-subject-id: test" -d "{ \"id\": \"$viewerRole\", \"groupName\": \"$viewerRole\"}" http://localhost:5004/groups/ 
-curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $accesstoken" -H "fabric-end-user-subject-id: test" -d "{ \"id\": \"$editorRole\", \"groupName\": \"$editorRole\"}" http://localhost:5004/groups/
+curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $accesstoken" -d "{ \"id\": \"$viewerRole\", \"groupName\": \"$viewerRole\"}" http://localhost:5004/groups/ 
+curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $accesstoken" -d "{ \"id\": \"$editorRole\", \"groupName\": \"$editorRole\"}" http://localhost:5004/groups/
