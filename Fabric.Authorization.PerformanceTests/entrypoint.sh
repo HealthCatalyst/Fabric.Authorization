db1c33dfbdb5b108f001e237eeb659353c117600
michael.vidal <michael.vidal@healthcatalyst.com>
2017-07-21 16:24:41 -0400

    Added a time limit to the performance tests. Added a Dockerfile that builds a container to run the performance tests.

---
+++ Fabric.Authorization.PerformanceTests/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+./create-userproperties.sh $AUTH_SERVER $AUTH_PORT $IDENTITY_SERVER $IDENTITY_PORT $BOB_PASSWORD $ALICE_PASSWORD
+mv user.properties /jmeter/apache-jmeter-3.2/bin/user.properties
+mkdir results
+mkdir results/output
+/jmeter/apache-jmeter-3.2/bin/jmeter -n -t /Fabric.Authorization.Perf.jmx -l /results/results.txt -e -o /results/output
+
