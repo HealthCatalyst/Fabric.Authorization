172ef07362e17a81dc1e9db6dab8502f6c1a2eb3
Kyle Paul <djjoebanks@gmail.com>
2017-08-17 14:15:27 -0700

    set environment variables to support generating markdown during build

--- Fabric.Authorization.API/scripts/set-environment.sh
+++ Fabric.Authorization.API/scripts/set-environment.sh
@@ -5,3 +5,8 @@ echo "CouchDb Container running on $IP"
 echo "##vso[task.setvariable variable=CouchDbSettings__Server;]http://$IP:5984"
 echo "CouchDb Server URL: $COUCHDBSETTINGS_SERVER"
 
+IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $AUTHORIZATIONCONTAINERNAME)
+echo "Authorization Container running on $IP"
+echo "##vso[task.setvariable variable=FabricAuthorizationBaseUrl;]http://$IP:5004"
+echo "Authorization Server URL (FABRICAUTHORIZATIONBASEURL): $FABRICAUTHORIZATIONBASEURL"
+echo "Authorization Server URL (FabricAuthorizationBaseUrl): $FabricAuthorizationBaseUrl"
