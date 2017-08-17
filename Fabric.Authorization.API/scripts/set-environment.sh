b8023339230b6d6e499773b3524c939b07b025a9
Kyle Paul <djjoebanks@gmail.com>
2017-08-17 14:30:27 -0700

    fixing  variable name in script

--- Fabric.Authorization.API/scripts/set-environment.sh
+++ Fabric.Authorization.API/scripts/set-environment.sh
@@ -2,5 +2,5 @@
 
 IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $COUCHDBCONTAINERNAME)
 echo "CouchDb Container running on $IP"
-echo "##vso[task.setvariable variable=CouchDbSettings__Server;]http://$IP:5984"
+echo "##vso[task.setvariable variable=CouchDbSettings_Server;]http://$IP:5984"
 echo "CouchDb Server URL: $COUCHDBSETTINGS_SERVER"
