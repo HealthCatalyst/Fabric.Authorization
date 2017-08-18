3bbdc57ae41b42df5ca7cf686208ceaa6c0f1790
Kyle Paul <djjoebanks@gmail.com>
2017-08-17 17:30:43 -0700

    updated script to set better names for api files

--- Fabric.Authorization.Api/scripts/add-docs-to-github-repo.sh
+++ Fabric.Authorization.Api/scripts/add-docs-to-github-repo.sh
@@ -4,6 +4,11 @@ echo "Cloning Git Wiki repo..."
 git clone https://$GITWIKIACCESSTOKEN@github.com/HealthCatalyst/Fabric.Authorization.wiki.git
 
 echo "Moving MD files to Fabric.Authorization.wiki..."
+mv overview.md API-Reference-Overview.md
+mv paths.md API-Reference-Resources.md
+mv definitions.md API-Reference-Models.md
+mv security.md API-Reference-Security.md
+
 mv *.md Fabric.Authorization.wiki
 
 echo "Changing directory..."
