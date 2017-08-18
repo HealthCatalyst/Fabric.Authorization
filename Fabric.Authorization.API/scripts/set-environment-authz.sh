ec5835fe8bbf727bee3bd514229232ae70c12415
Kyle Paul <djjoebanks@gmail.com>
2017-08-18 08:28:05 -0700

    update line endings

--- Fabric.Authorization.API/scripts/set-environment-authz.sh
+++ Fabric.Authorization.API/scripts/set-environment-authz.sh
@@ -2,6 +2,6 @@
 
 AUTHZIP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $AUTHORIZATIONCONTAINERNAME)
 echo "Authorization Container running on $AUTHZIP"
-echo "##vso[task.setvariable variable=FabricAuthorizationBaseUrl;]http://$AUTHZIP:5004"
+echo "##vso[task.setvariable variable=FabricAuthorizationBaseUrl;]http://$AUTHZIP:5004" 
 echo "Authorization Server URL (FABRICAUTHORIZATIONBASEURL): $FABRICAUTHORIZATIONBASEURL"
 echo "Authorization Server URL (FabricAuthorizationBaseUrl): $FabricAuthorizationBaseUrl"
