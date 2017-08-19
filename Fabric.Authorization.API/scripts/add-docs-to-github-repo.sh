010faa187217ce6b04bb437637ceb5deae24ec72
Kyle Paul <djjoebanks@gmail.com>
2017-08-19 10:51:24 -0700

    updated email for git account

--- Fabric.Authorization.API/scripts/add-docs-to-github-repo.sh
+++ Fabric.Authorization.API/scripts/add-docs-to-github-repo.sh
@@ -17,7 +17,7 @@ cd Fabric.Authorization.wiki
 echo "-----Present directory = $(pwd)-----"
 
 git config user.name "vsts build"
-git config user.email "kyle.paul@healthcatalyst.com"
+git config user.email "djjoebanks@gmail.com"
 git add *.md
 git commit -m 'update API documentation'
 git push origin master
