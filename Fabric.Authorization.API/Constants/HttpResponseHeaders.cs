7bfe0733a5f982c8a764c28d1c1578ad9b4e2a71
Kyle Paul <djjoebanks@gmail.com>
2017-09-19 15:50:50 -0700

    move cors headers to HttpResponseHeaders file

--- Fabric.Authorization.API/Constants/HttpResponseHeaders.cs
+++ Fabric.Authorization.API/Constants/HttpResponseHeaders.cs
@@ -1,7 +1,15 @@
-﻿namespace Fabric.Authorization.API.Constants
+﻿using System;
+
+namespace Fabric.Authorization.API.Constants
 {
     public static class HttpResponseHeaders
     {
         public static readonly string Location = "Location";
+        public static readonly Tuple<string, string>[] CorsHeaders = 
+        {
+            new Tuple<string, string>("Access-Control-Allow-Origin", "*"),
+            new Tuple<string, string>("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization"),  
+            new Tuple<string, string>("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE") 
+        };
     }
 }
