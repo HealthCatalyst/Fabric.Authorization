cc04342bbdd32d856a66adb8678bc400caadf531
Brian Smith <brian.smith@healthcatalyst.com>
2017-09-19 03:04:42 -0400

    removed JsonProperty attributes since the custom model binder handles deserialization

--- Fabric.Authorization.API/Models/Search/IdentitySearchRequest.cs
+++ Fabric.Authorization.API/Models/Search/IdentitySearchRequest.cs
@@ -4,7 +4,6 @@ namespace Fabric.Authorization.API.Models.Search
 {
     public class IdentitySearchRequest : SearchRequest
     {
-        [JsonProperty("client_id")]
         public string ClientId { get; set; }
     }
 }
--- Fabric.Authorization.API/Models/Search/SearchRequest.cs
+++ Fabric.Authorization.API/Models/Search/SearchRequest.cs
@@ -4,18 +4,10 @@ namespace Fabric.Authorization.API.Models.Search
 {
     public abstract class SearchRequest
     {
-        [JsonProperty("page_number")]
         public int PageNumber { get; set; }
-
-        [JsonProperty("page_size")]
         public int PageSize { get; set; }
-
         public string Filter { get; set; }
-
-        [JsonProperty("sort_key")]
         public string SortKey { get; set; }
-
-        [JsonProperty("sort_dir")]
         public string SortDirection { get; set; }
     }
 }
