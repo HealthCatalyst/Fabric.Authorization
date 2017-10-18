497384adf3895f759ac6f0ea6dc753044f91fb20
Brian Smith <brian.smith@healthcatalyst.com>
2017-10-18 12:11:22 -0400

    added permission resolvers to encapsulate permission calculation logic for different scenarios

---
+++ Fabric.Authorization.Domain/Models/PermissionResolutionRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace Fabric.Authorization.Domain.Models
+{
+    public class PermissionResolutionRequest
+    {
+        public string Grain { get; set; }
+        public string SecurableItem { get; set; }
+        public string IdentityProvider { get; set; }
+        public string SubjectId { get; set; }
+    }
+}
---
+++ Fabric.Authorization.Domain/Resolvers/Permissions/IPermissionResolver.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Fabric.Authorization.Domain.Models;
+
+namespace Fabric.Authorization.Domain.Resolvers.Permissions
+{
+    public interface IPermissionResolver
+    {
+        IEnumerable<Permission> Resolve(PermissionResolutionRequest resolutionRequest);
+    }
+}
