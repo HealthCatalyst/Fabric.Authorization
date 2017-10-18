3ad1d587c0925299b9a41cda78a561e27b559876
Brian Smith <brian.smith@healthcatalyst.com>
2017-10-18 12:11:22 -0400

    added permission resolvers to encapsulate permission calculation logic for different scenarios

---
+++ Fabric.Authorization.Domain/Resolvers/Permissions/DetailedPermissionResolver.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Fabric.Authorization.Domain.Models;
+using Fabric.Authorization.Domain.Stores.Services;
+
+namespace Fabric.Authorization.Domain.Resolvers.Permissions
+{
+    public class DetailedPermissionResolver : IPermissionResolver
+    {
+        protected readonly GroupService GroupService;
+        protected readonly RoleService RoleService;
+        protected readonly UserService UserService;
+
+        public DetailedPermissionResolver(
+            UserService userService,
+            GroupService groupService,
+            RoleService roleService)
+        {
+            UserService = userService;
+            GroupService = groupService;
+            RoleService = roleService;
+        }
+
+        protected IEnumerable<Permission> AllowedPermissions => new List<Permission>();
+
+        protected IEnumerable<Permission> DeniedPermissions => new List<Permission>();
+
+        public virtual IEnumerable<Permission> Resolve(PermissionResolutionRequest resolutionRequest)
+        {
+            return new List<Permission>();
+        }
+    }
+}
---
+++ Fabric.Authorization.Domain/Resolvers/Permissions/EffectivePermissionResolver.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Fabric.Authorization.Domain.Models;
+using Fabric.Authorization.Domain.Stores.Services;
+
+namespace Fabric.Authorization.Domain.Resolvers.Permissions
+{
+    public class EffectivePermissionResolver : DetailedPermissionResolver
+    {
+        public EffectivePermissionResolver(
+            UserService userService,
+            GroupService groupService,
+            RoleService roleService) : base(userService, groupService, roleService)
+        {
+        }
+
+        public override IEnumerable<Permission> Resolve(PermissionResolutionRequest resolutionRequest)
+        {
+            return AllowedPermissions.Except(DeniedPermissions).Distinct();
+        }
+    }
+}
