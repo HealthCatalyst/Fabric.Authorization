23474ae3b50909198d232afcc3facce34a4a33bd
Brian Smith <brian.smith@healthcatalyst.com>
2017-10-19 21:05:59 -0400

    moved extension methods to separate file

---
+++ Fabric.Authorization.Domain/Resolvers/Models/ResolvedPermissionExtensions..cs
@@ -0,0 +1,33 @@
+﻿using Fabric.Authorization.Domain.Models;
+
+namespace Fabric.Authorization.Domain.Resolvers.Models
+{
+    public static class ResolvedPermissionExtensions
+    {
+        public static ResolvedPermission ToResolvedPermission(this Permission permission, string action,
+            Role role = null)
+        {
+            return new ResolvedPermission
+            {
+                Id = permission.Id,
+                Grain = permission.Grain,
+                SecurableItem = permission.SecurableItem,
+                Name = permission.Name,
+                Action = action,
+                CreatedDateTimeUtc = permission.CreatedDateTimeUtc,
+                ModifiedDateTimeUtc = permission.ModifiedDateTimeUtc,
+                CreatedBy = permission.CreatedBy,
+                ModifiedBy = permission.ModifiedBy
+            };
+        }
+
+        public static ResolvedPermissionRole ToResolvedPermissionRole(this Role role)
+        {
+            return new ResolvedPermissionRole
+            {
+                Id = role.Id,
+                Name = role.Name
+            };
+        }
+    }
+}
