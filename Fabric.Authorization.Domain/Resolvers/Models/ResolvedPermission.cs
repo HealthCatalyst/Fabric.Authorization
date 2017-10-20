b9ab8bc9014e29b82a55208f634d11c6b095143b
Brian Smith <brian.smith@healthcatalyst.com>
2017-10-19 21:05:59 -0400

    moved extension methods to separate file

--- Fabric.Authorization.Domain/Resolvers/Models/ResolvedPermission.cs
+++ Fabric.Authorization.Domain/Resolvers/Models/ResolvedPermission.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using Fabric.Authorization.Domain.Models;
 
 namespace Fabric.Authorization.Domain.Resolvers.Models
 {
@@ -45,34 +44,6 @@ namespace Fabric.Authorization.Domain.Resolvers.Models
         public override int GetHashCode()
         {
             return ToString().GetHashCode();
-        }
-    }
-
-    public static class ResolvedPermissionExtensions
-    {
-        public static ResolvedPermission ToResolvedPermission(this Permission permission, string action, Role role = null)
-        {
-            return new ResolvedPermission
-            {
-                Id = permission.Id,
-                Grain = permission.Grain,
-                SecurableItem = permission.SecurableItem,
-                Name = permission.Name,
-                Action = action,
-                CreatedDateTimeUtc = permission.CreatedDateTimeUtc,
-                ModifiedDateTimeUtc = permission.ModifiedDateTimeUtc,
-                CreatedBy = permission.CreatedBy,
-                ModifiedBy = permission.ModifiedBy
-            };
-        }
-
-        public static ResolvedPermissionRole ToResolvedPermissionRole(this Role role)
-        {
-            return new ResolvedPermissionRole
-            {
-                Id = role.Id,
-                Name = role.Name
-            };
         }
     }
 }
