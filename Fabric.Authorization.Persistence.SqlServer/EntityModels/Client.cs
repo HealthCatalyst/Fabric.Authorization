3815718aa7aeb670f3746423834d17af949ad476
Kyle Paul <djjoebanks@gmail.com>
2017-12-13 13:55:47 -0800

    add client and securableitem entity models

---
+++ Fabric.Authorization.Persistence.SqlServer/EntityModels/Client.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Fabric.Authorization.Domain.Models;
+
+namespace Fabric.Authorization.Persistence.SqlServer.EntityModels
+{
+    public class Client : ITrackable, ISoftDelete
+    {
+        public int Id { get; set; }
+        public string ClientId { get; set; }
+        public string Name { get; set; }
+        public int SecurableItemId { get; set; }
+        
+        public DateTime CreatedDateTimeUtc { get; set; }
+        public DateTime? ModifiedDateTimeUtc { get; set; }
+        public string CreatedBy { get; set; }
+        public string ModifiedBy { get; set; }
+        public bool IsDeleted { get; set; }
+
+        public SecurableItem TopLevelSecurableItem { get; set; }
+    }
+}
---
+++ Fabric.Authorization.Persistence.SqlServer/EntityModels/SecurableItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Fabric.Authorization.Domain.Models;
+
+namespace Fabric.Authorization.Persistence.SqlServer.EntityModels
+{
+    public class SecurableItem : ITrackable, ISoftDelete
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public DateTime CreatedDateTimeUtc { get; set; }
+        public DateTime? ModifiedDateTimeUtc { get; set; }
+        public string CreatedBy { get; set; }
+        public string ModifiedBy { get; set; }
+        public bool IsDeleted { get; set; }
+
+
+    }
+}
