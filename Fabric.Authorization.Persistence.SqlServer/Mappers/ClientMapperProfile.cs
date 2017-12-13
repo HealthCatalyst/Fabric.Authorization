95c9c1e7e3b246ab53cae93c35527ff028ba4aae
Kyle Paul <djjoebanks@gmail.com>
2017-12-13 15:53:49 -0800

    add automapper profile and mapper for client. add client db set to db context

---
+++ Fabric.Authorization.Persistence.SqlServer/Mappers/ClientMapperProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+
+namespace Fabric.Authorization.Persistence.SqlServer.Mappers
+{
+    public class ClientMapperProfile : Profile
+    {
+        public ClientMapperProfile()
+        {
+            //entity to model 
+            CreateMap<EntityModels.Client, Domain.Models.Client>()
+                .ForMember(x => x.Id, opt => opt.MapFrom(src => src.ClientId))
+                .ReverseMap();
+
+            //model to entity
+            //CreateMap<Domain.Models.Client, EntityModels.Client>()
+            //    .ForMember(x => x.ClientId, opt => opt.MapFrom(src => src.Id));
+        }
+    }
+}
