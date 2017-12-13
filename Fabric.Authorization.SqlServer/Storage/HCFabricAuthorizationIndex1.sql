51095a7ceead1fd184566c1ad50dc94a06e1676c
Kyle Paul <djjoebanks@gmail.com>
2017-12-13 09:50:10 -0800

    added sql scripts for specifying file locations for the mdf, ndf, and log files

---
+++ Fabric.Authorization.SqlServer/Storage/HCFabricAuthorizationIndex1.sql
@@ -0,0 +1,22 @@
+﻿/*
+Do not change the database path or name variables.
+Any sqlcmd variables will be properly substituted during 
+build and deployment.
+*/
+ALTER DATABASE [$(DatabaseName)]
+	ADD FILEGROUP [HCFabricAuthorizationIndex1]
+
+	GO
+
+ALTER DATABASE [$(DatabaseName)]
+	ADD FILE
+	(
+		NAME = [HCFabricAuthorizationIndex1File1],
+		FILENAME = '$(FabricAuthorizationDataMountPoint)\HC$(DatabaseName)Index1File1.ldf',
+		SIZE = 500MB,
+		MAXSIZE = 2GB,
+		FILEGROWTH = 100MB
+	)
+
+TO FILEGROUP [HCFabricAuthorizationIndex1];
+GO
---
+++ Fabric.Authorization.SqlServer/Storage/HCFabricAuthorizationPrimary.sql
@@ -0,0 +1,11 @@
+﻿/*
+Do not change the database path or name variables.
+Any sqlcmd variables will be properly substituted during 
+build and deployment.
+*/
+ALTER DATABASE [$(DatabaseName)]
+	ADD FILE
+	(
+		NAME = [HCFabricAuthorizationPrimary],
+		FILENAME = '$(FabricAuthorizationDataMountPoint)\HC$(DatabaseName)Primary.mdf'
+	)
