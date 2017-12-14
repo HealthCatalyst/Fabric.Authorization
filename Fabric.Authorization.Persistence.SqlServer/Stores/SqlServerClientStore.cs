eb10eda2e7ae9391c84562c9d6e9d78bdc9324d3
Kyle Paul <djjoebanks@gmail.com>
2017-12-14 11:16:42 -0800

    implement IClientStore methods

--- Fabric.Authorization.Persistence.SqlServer/Stores/SqlServerClientStore.cs
+++ Fabric.Authorization.Persistence.SqlServer/Stores/SqlServerClientStore.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 using System.Threading.Tasks;
+using Fabric.Authorization.Domain.Exceptions;
 using Fabric.Authorization.Domain.Models;
 using Fabric.Authorization.Domain.Stores;
+using Fabric.Authorization.Persistence.SqlServer.Mappers;
 using Fabric.Authorization.Persistence.SqlServer.Services;
+using Microsoft.EntityFrameworkCore;
 
 namespace Fabric.Authorization.Persistence.SqlServer.Stores
 {
@@ -17,34 +20,83 @@ namespace Fabric.Authorization.Persistence.SqlServer.Stores
             _authorizationDbContext = authorizationDbContext;
         }
 
-        public Task<Client> Add(Client model)
+        public async Task<Client> Add(Client model)
         {
-            throw new NotImplementedException();
+            var clientEntity = model.ToEntity();
+
+            _authorizationDbContext.Clients.Add(clientEntity);
+            await _authorizationDbContext.SaveChangesAsync();
+            return model;
         }
 
-        public Task<Client> Get(string id)
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id">This is the unique client id of the client</param>
+        /// <returns></returns>
+        public async Task<Client> Get(string id)
         {
-            throw new NotImplementedException();
+            var client = await _authorizationDbContext.Clients
+                .Include(i => i.TopLevelSecurableItem)
+                .SingleOrDefaultAsync(c => c.ClientId.Equals(id, StringComparison.OrdinalIgnoreCase)
+                                           && !c.IsDeleted);
+
+            if (client == null)
+            {
+                throw new NotFoundException<Client>($"Could not find {typeof(Client).Name} entity with ID {id}");
+            }
+
+            return client.ToModel();
         }
 
-        public Task<IEnumerable<Client>> GetAll()
+        public async Task<IEnumerable<Client>> GetAll()
         {
-            throw new NotImplementedException();
+            var clients = await _authorizationDbContext.Clients
+                .Where(c => !c.IsDeleted)
+                .ToArrayAsync();
+
+            return clients.Select(c => c.ToModel());
         }
 
-        public Task Delete(Client model)
+        public async Task Delete(Client model)
         {
-            throw new NotImplementedException();
+            var client = await _authorizationDbContext.Clients
+                .Include(i => i.TopLevelSecurableItem)
+                .SingleOrDefaultAsync(c => c.ClientId.Equals(model.Id, StringComparison.OrdinalIgnoreCase)
+                                           && !c.IsDeleted);
+
+            client.IsDeleted = true;
+            client.TopLevelSecurableItem.IsDeleted = true;
+
+            foreach (var securableItem in client.TopLevelSecurableItem.SecurableItems)
+            {
+                securableItem.IsDeleted = true;
+            }
+
+            await _authorizationDbContext.SaveChangesAsync();
         }
 
-        public Task Update(Client model)
+        public async Task Update(Client model)
         {
-            throw new NotImplementedException();
+            var client = await _authorizationDbContext.Clients
+                .Include(i => i.TopLevelSecurableItem)
+                .SingleOrDefaultAsync(c => c.ClientId.Equals(model.Id, StringComparison.OrdinalIgnoreCase)
+                                           && !c.IsDeleted);
+
+            model.ToEntity(client);
+
+            _authorizationDbContext.Clients.Update(client);
+            await _authorizationDbContext.SaveChangesAsync();
         }
 
-        public Task<bool> Exists(string id)
+        public async Task<bool> Exists(string id)
         {
-            throw new NotImplementedException();
+            var client = await _authorizationDbContext.Clients
+                .Include(i => i.TopLevelSecurableItem)
+                .SingleOrDefaultAsync(c => c.ClientId.Equals(id, StringComparison.OrdinalIgnoreCase)
+                                           && !c.IsDeleted);
+
+            return client != null;
         }
     }
 }
