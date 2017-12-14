cca34a4093b3d08c1cc78c93b75362b1731aa146
Brian Smith <brian.smith@healthcatalyst.com>
2017-12-14 10:40:07 -0500

    added GroupRole entity model

---
+++ Fabric.Authorization.Persistence.SqlServer/EntityModels/GroupRole.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Fabric.Authorization.Domain.Models;
+
+namespace Fabric.Authorization.Persistence.SqlServer.EntityModels
+{
+    public class GroupRole : ITrackable, ISoftDelete
+    {
+        public int GroupId { get; set; }
+        public int RoleId { get; set; }
+
+        public DateTime CreatedDateTimeUtc { get; set; }
+        public DateTime? ModifiedDateTimeUtc { get; set; }
+        public string CreatedBy { get; set; }
+        public string ModifiedBy { get; set; }
+        public bool IsDeleted { get; set; }
+
+        public Group Group { get; set; }
+        public Role Role { get; set; }
+    }
+}
