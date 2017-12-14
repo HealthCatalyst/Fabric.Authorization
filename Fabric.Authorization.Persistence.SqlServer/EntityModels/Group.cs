27d390586fc16a56472b34ae32f244621f93fc5a
Brian Smith <brian.smith@healthcatalyst.com>
2017-12-14 10:39:23 -0500

    added Group entity model

---
+++ Fabric.Authorization.Persistence.SqlServer/EntityModels/Group.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Fabric.Authorization.Domain.Models;
+
+namespace Fabric.Authorization.Persistence.SqlServer.EntityModels
+{
+    public class Group : ITrackable, ISoftDelete
+    {
+        public Group()
+        {
+            Roles = new List<Role>();
+            //Users = new List<User>();
+        }
+        public int Id { get; set; }
+        public Guid ExternalIdentifier { get; set; }
+        public string Name { get; set; }
+        public string Source { get; set; }
+
+        public DateTime CreatedDateTimeUtc { get; set; }
+        public DateTime? ModifiedDateTimeUtc { get; set; }
+        public string CreatedBy { get; set; }
+        public string ModifiedBy { get; set; }
+        public bool IsDeleted { get; set; }
+
+        public ICollection<Role> Roles { get; set; }
+        //public ICollection<User> Users { get; set; }
+    }
+}
