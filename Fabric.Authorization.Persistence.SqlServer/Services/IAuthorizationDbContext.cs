773e9b3c70b1f41de3c16cea8c940fd00cc62ef3
Brian Smith <brian.smith@healthcatalyst.com>
2017-12-14 14:06:35 -0500

    add Permission and User entity models

--- Fabric.Authorization.Persistence.SqlServer/Services/IAuthorizationDbContext.cs
+++ Fabric.Authorization.Persistence.SqlServer/Services/IAuthorizationDbContext.cs
@@ -1,6 +1,12 @@
 ﻿using System.Threading.Tasks;
 using Fabric.Authorization.Persistence.SqlServer.EntityModels;
 using Microsoft.EntityFrameworkCore;
+using Client = Fabric.Authorization.Persistence.SqlServer.EntityModels.Client;
+using Group = Fabric.Authorization.Persistence.SqlServer.EntityModels.Group;
+using Permission = Fabric.Authorization.Persistence.SqlServer.EntityModels.Permission;
+using Role = Fabric.Authorization.Persistence.SqlServer.EntityModels.Role;
+using SecurableItem = Fabric.Authorization.Persistence.SqlServer.EntityModels.SecurableItem;
+using User = Fabric.Authorization.Persistence.SqlServer.EntityModels.User;
 
 namespace Fabric.Authorization.Persistence.SqlServer.Services
 {
@@ -8,7 +14,12 @@ namespace Fabric.Authorization.Persistence.SqlServer.Services
     {
         DbSet<Client> Clients { get; set; }
         DbSet<SecurableItem> SecurableItems { get; set; }
-
+        DbSet<Group> Groups { get; set; }
+        DbSet<Role> Roles { get; set; }
+        DbSet<GroupRole> GroupRoles { get; set; }
+        DbSet<Permission> Permissions { get; set; }
+        DbSet<RolePermission> RolePermissions { get; set; }
+        DbSet<User> Users { get; set; }
         Task<int> SaveChangesAsync();
         int SaveChanges();
     }
