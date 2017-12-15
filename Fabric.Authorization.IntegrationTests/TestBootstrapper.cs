d6724260086f667c11ceefe61a459dee1f6aff1f
michael.vidal <michael.vidal@healthcatalyst.com>
2017-12-14 21:36:25 -0500

    Updated IntegrationTestsFixture to use TestBootstrapper which wraps the normal Bootstrapper to start up test server. Updated UserTests to use the new Browser.

---
+++ Fabric.Authorization.IntegrationTests/TestBootstrapper.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Claims;
+using Fabric.Authorization.API;
+using Fabric.Authorization.API.Configuration;
+using Microsoft.AspNetCore.Hosting;
+using Nancy;
+using Nancy.Bootstrapper;
+using Nancy.TinyIoc;
+using Serilog;
+using Serilog.Core;
+
+namespace Fabric.Authorization.IntegrationTests
+{
+    public class TestBootstrapper : Bootstrapper
+    {
+        private readonly ClaimsPrincipal _principal;
+        public TestBootstrapper(ILogger logger, IAppConfiguration appConfig, LoggingLevelSwitch levelSwitch, IHostingEnvironment env, ClaimsPrincipal principal) : base(logger, appConfig, levelSwitch, env)
+        {
+            _principal = principal;
+        }
+
+        protected override void RequestStartup(TinyIoCContainer container, IPipelines pipelines, NancyContext context)
+        {
+            base.RequestStartup(container, pipelines, context);
+            context.CurrentUser = _principal;
+        }
+    }
+}
