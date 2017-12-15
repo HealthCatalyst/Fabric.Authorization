12041d4928bc52a941885cd3d8c0487a133035d0
Brian Smith <brian.smith@healthcatalyst.com>
2017-12-15 16:14:14 -0500

    fixed model definitions based on bugs found in migration script

---
+++ Fabric.Authorization.Persistence.SqlServer/Migrations/20171215210021_Test.cs
@@ -0,0 +1,379 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Fabric.Authorization.Persistence.SqlServer.Migrations
+{
+    public partial class Test : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Groups",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CreatedBy = table.Column<string>(maxLength: 100, nullable: false),
+                    CreatedDateTimeUtc = table.Column<DateTime>(type: "datetime", nullable: false),
+                    GroupId = table.Column<string>(nullable: true),
+                    IsDeleted = table.Column<bool>(nullable: false, defaultValueSql: "0"),
+                    ModifiedBy = table.Column<string>(nullable: true),
+                    ModifiedDateTimeUtc = table.Column<DateTime>(type: "datetime", nullable: true),
+                    Name = table.Column<string>(maxLength: 200, nullable: false),
+                    Source = table.Column<string>(maxLength: 200, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Groups", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SecurableItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CreatedBy = table.Column<string>(maxLength: 100, nullable: false),
+                    CreatedDateTimeUtc = table.Column<DateTime>(type: "datetime", nullable: false),
+                    IsDeleted = table.Column<bool>(nullable: false, defaultValueSql: "0"),
+                    ModifiedBy = table.Column<string>(nullable: true),
+                    ModifiedDateTimeUtc = table.Column<DateTime>(nullable: true),
+                    Name = table.Column<string>(maxLength: 200, nullable: false),
+                    SecurableItemId = table.Column<Guid>(nullable: false),
+                    SecurableItemId1 = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SecurableItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SecurableItems_SecurableItems_SecurableItemId1",
+                        column: x => x.SecurableItemId1,
+                        principalTable: "SecurableItems",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CreatedBy = table.Column<string>(maxLength: 100, nullable: false),
+                    CreatedDateTimeUtc = table.Column<DateTime>(type: "datetime", nullable: false),
+                    IdentityProvider = table.Column<string>(maxLength: 200, nullable: false),
+                    IsDeleted = table.Column<bool>(nullable: false, defaultValueSql: "0"),
+                    ModifiedBy = table.Column<string>(nullable: true),
+                    ModifiedDateTimeUtc = table.Column<DateTime>(type: "datetime", nullable: true),
+                    Name = table.Column<string>(maxLength: 200, nullable: false),
+                    SubjectId = table.Column<string>(maxLength: 200, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Clients",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    ClientId = table.Column<string>(nullable: true),
+                    CreatedBy = table.Column<string>(maxLength: 100, nullable: false),
+                    CreatedDateTimeUtc = table.Column<DateTime>(type: "datetime", nullable: false),
+                    IsDeleted = table.Column<bool>(nullable: false, defaultValueSql: "0"),
+                    ModifiedBy = table.Column<string>(nullable: true),
+                    ModifiedDateTimeUtc = table.Column<DateTime>(nullable: true),
+                    Name = table.Column<string>(maxLength: 200, nullable: false),
+                    SecurableItemId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clients", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Clients_SecurableItems_SecurableItemId",
+                        column: x => x.SecurableItemId,
+                        principalTable: "SecurableItems",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Permissions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CreatedBy = table.Column<string>(maxLength: 100, nullable: false),
+                    CreatedDateTimeUtc = table.Column<DateTime>(type: "datetime", nullable: false),
+                    Grain = table.Column<string>(maxLength: 200, nullable: false),
+                    IsDeleted = table.Column<bool>(nullable: false, defaultValueSql: "0"),
+                    ModifiedBy = table.Column<string>(nullable: true),
+                    ModifiedDateTimeUtc = table.Column<DateTime>(type: "datetime", nullable: true),
+                    Name = table.Column<string>(maxLength: 200, nullable: false),
+                    PermissionId = table.Column<Guid>(nullable: false),
+                    SecurableItemId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Permissions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Permissions_SecurableItems_SecurableItemId",
+                        column: x => x.SecurableItemId,
+                        principalTable: "SecurableItems",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Roles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CreatedBy = table.Column<string>(maxLength: 100, nullable: false),
+                    CreatedDateTimeUtc = table.Column<DateTime>(type: "datetime", nullable: false),
+                    Grain = table.Column<string>(maxLength: 200, nullable: false),
+                    IsDeleted = table.Column<bool>(nullable: false, defaultValueSql: "0"),
+                    ModifiedBy = table.Column<string>(nullable: true),
+                    ModifiedDateTimeUtc = table.Column<DateTime>(type: "datetime", nullable: true),
+                    Name = table.Column<string>(maxLength: 200, nullable: false),
+                    ParentRoleId = table.Column<int>(nullable: true),
+                    RoleId = table.Column<Guid>(nullable: false),
+                    SecurableItemId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Roles", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Roles_Roles_ParentRoleId",
+                        column: x => x.ParentRoleId,
+                        principalTable: "Roles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Roles_SecurableItems_SecurableItemId",
+                        column: x => x.SecurableItemId,
+                        principalTable: "SecurableItems",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GroupUsers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CreatedBy = table.Column<string>(nullable: true),
+                    CreatedDateTimeUtc = table.Column<DateTime>(nullable: false),
+                    GroupId = table.Column<int>(nullable: false),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    ModifiedBy = table.Column<string>(nullable: true),
+                    ModifiedDateTimeUtc = table.Column<DateTime>(nullable: true),
+                    UserId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GroupUsers", x => x.Id);
+                    table.UniqueConstraint("AK_GroupUsers_UserId_GroupId", x => new { x.UserId, x.GroupId });
+                    table.ForeignKey(
+                        name: "FK_GroupUsers_Groups_GroupId",
+                        column: x => x.GroupId,
+                        principalTable: "Groups",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_GroupUsers_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserPermissions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CreatedBy = table.Column<string>(nullable: true),
+                    CreatedDateTimeUtc = table.Column<DateTime>(nullable: false),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    ModifiedBy = table.Column<string>(nullable: true),
+                    ModifiedDateTimeUtc = table.Column<DateTime>(nullable: true),
+                    PermissionAction = table.Column<int>(nullable: false, defaultValueSql: "0"),
+                    PermissionId = table.Column<int>(nullable: false),
+                    UserId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserPermissions", x => x.Id);
+                    table.UniqueConstraint("AK_UserPermissions_UserId_PermissionId", x => new { x.UserId, x.PermissionId });
+                    table.ForeignKey(
+                        name: "FK_UserPermissions_Permissions_PermissionId",
+                        column: x => x.PermissionId,
+                        principalTable: "Permissions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserPermissions_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GroupRoles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CreatedBy = table.Column<string>(nullable: true),
+                    CreatedDateTimeUtc = table.Column<DateTime>(nullable: false),
+                    GroupId = table.Column<int>(nullable: false),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    ModifiedBy = table.Column<string>(nullable: true),
+                    ModifiedDateTimeUtc = table.Column<DateTime>(nullable: true),
+                    RoleId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GroupRoles", x => x.Id);
+                    table.UniqueConstraint("AK_GroupRoles_GroupId_RoleId", x => new { x.GroupId, x.RoleId });
+                    table.ForeignKey(
+                        name: "FK_GroupRoles_Groups_GroupId",
+                        column: x => x.GroupId,
+                        principalTable: "Groups",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_GroupRoles_Roles_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "Roles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RolePermissions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CreatedBy = table.Column<string>(nullable: true),
+                    CreatedDateTimeUtc = table.Column<DateTime>(nullable: false),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    ModifiedBy = table.Column<string>(nullable: true),
+                    ModifiedDateTimeUtc = table.Column<DateTime>(nullable: true),
+                    PermissionAction = table.Column<int>(nullable: false, defaultValueSql: "0"),
+                    PermissionId = table.Column<int>(nullable: false),
+                    RoleId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RolePermissions", x => x.Id);
+                    table.UniqueConstraint("AK_RolePermissions_RoleId_PermissionId", x => new { x.RoleId, x.PermissionId });
+                    table.ForeignKey(
+                        name: "FK_RolePermissions_Permissions_PermissionId",
+                        column: x => x.PermissionId,
+                        principalTable: "Permissions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_RolePermissions_Roles_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "Roles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Clients_SecurableItemId",
+                table: "Clients",
+                column: "SecurableItemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GroupRoles_RoleId",
+                table: "GroupRoles",
+                column: "RoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GroupUsers_GroupId",
+                table: "GroupUsers",
+                column: "GroupId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Permissions_SecurableItemId",
+                table: "Permissions",
+                column: "SecurableItemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Roles_ParentRoleId",
+                table: "Roles",
+                column: "ParentRoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Roles_SecurableItemId",
+                table: "Roles",
+                column: "SecurableItemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RolePermissions_PermissionId",
+                table: "RolePermissions",
+                column: "PermissionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SecurableItems_SecurableItemId",
+                table: "SecurableItems",
+                column: "SecurableItemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SecurableItems_SecurableItemId1",
+                table: "SecurableItems",
+                column: "SecurableItemId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserPermissions_PermissionId",
+                table: "UserPermissions",
+                column: "PermissionId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clients");
+
+            migrationBuilder.DropTable(
+                name: "GroupRoles");
+
+            migrationBuilder.DropTable(
+                name: "GroupUsers");
+
+            migrationBuilder.DropTable(
+                name: "RolePermissions");
+
+            migrationBuilder.DropTable(
+                name: "UserPermissions");
+
+            migrationBuilder.DropTable(
+                name: "Groups");
+
+            migrationBuilder.DropTable(
+                name: "Roles");
+
+            migrationBuilder.DropTable(
+                name: "Permissions");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "SecurableItems");
+        }
+    }
+}
