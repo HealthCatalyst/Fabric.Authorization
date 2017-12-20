194425d5c8d07633228ea83f062f47252895f57d
michael.vidal <michael.vidal@healthcatalyst.com>
2017-12-20 14:23:05 -0500

    Added the StorageProvider config setting.

---
+++ Fabric.Authorization.API/Constants/StorageProviders.cs
@@ -0,0 +1,9 @@
+﻿namespace Fabric.Authorization.API.Constants
+{
+    public static class StorageProviders
+    {
+        public const string InMemory = "inmemory";
+        public const string CouchDb = "coucdb";
+        public const string SqlServer = "sqlserver";
+    }
+}
