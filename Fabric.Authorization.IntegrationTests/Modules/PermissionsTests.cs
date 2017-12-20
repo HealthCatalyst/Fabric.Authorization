db872fa56717f8c0685f31eb94bc006724cffdb1
michael.vidal <michael.vidal@healthcatalyst.com>
2017-12-20 12:06:34 -0500

    Convert PermissionTests to create a single database.

--- Fabric.Authorization.IntegrationTests/Modules/PermissionsTests.cs
+++ Fabric.Authorization.IntegrationTests/Modules/PermissionsTests.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Security.Claims;
 using Fabric.Authorization.API.Constants;
-using Fabric.Authorization.Domain.Stores;
 using Nancy;
 using Nancy.Testing;
 using Xunit;
@@ -10,40 +9,33 @@ using Xunit;
 namespace Fabric.Authorization.IntegrationTests.Modules
 {
     [Collection("InMemoryTests")]
-    public class PermissionsTests : IntegrationTestsFixture
+    public class PermissionsTests : IClassFixture<IntegrationTestsFixture>
     {
-        private readonly IIdentifierFormatter _identifierFormatter = new IdpIdentifierFormatter();
+        private readonly Browser _browser;
+        private readonly string _securableItem;
 
-        public PermissionsTests(bool useInMemoryDB = true)
+        public PermissionsTests(IntegrationTestsFixture fixture, bool useInMemoryDb = true)
         {
+            _securableItem = "permissionprincipal" + Guid.NewGuid();
             var principal = new ClaimsPrincipal(new ClaimsIdentity(new List<Claim>
             {
                 new Claim(Claims.Scope, Scopes.ManageClientsScope),
                 new Claim(Claims.Scope, Scopes.ReadScope),
                 new Claim(Claims.Scope, Scopes.WriteScope),
-                new Claim(Claims.ClientId, "permissionprincipal")
-            }, "permissionprincipal"));
+                new Claim(Claims.ClientId, _securableItem)
+            }, _securableItem));
 
-            Browser = GetBrowser(principal, useInMemoryDB);
-
-            Browser.Post("/clients", with =>
-            {
-                with.HttpRequest();
-                with.JsonBody(new
-                {
-                    Id = "permissionprincipal",
-                    Name = "permissionprincipal"
-                });
-            }).Wait();
+            _browser = fixture.GetBrowser(principal, useInMemoryDb);
+            fixture.CreateClient(_browser, _securableItem);
         }
 
         [Theory]
-        [DisplayTestMethodName]
-        [InlineData("InexistentPermission")]
-        [InlineData("InexistentPermission2")]
+        [IntegrationTestsFixture.DisplayTestMethodName]
+        [InlineData("986201B4-F593-44CA-882E-ABD32DA26DF9")]
+        [InlineData("21967D2D-6B95-4461-8B2D-6398303262B0")]
         public void TestGetPermission_Fail(string permission)
         {
-            var get = Browser.Get($"/permissions/app/permissionprincipal/{permission}", with =>
+            var get = _browser.Get($"/permissions/app/{_securableItem}/{permission}", with =>
             {
                 with.HttpRequest();
             }).Result;
@@ -53,24 +45,24 @@ namespace Fabric.Authorization.IntegrationTests.Modules
         }
 
         [Theory]
-        [DisplayTestMethodName]
-        [InlineData("Perm1")]
-        [InlineData("Perm2")]
+        [IntegrationTestsFixture.DisplayTestMethodName]
+        [InlineData("E795EFB0-94D1-40CD-A317-BCDE60C9C30A")]
+        [InlineData("0E2C3C9E-D593-4941-9750-7EF3B11C4BFE")]
         public void TestAddNewPermission_Success(string permission)
         {
-            var postResponse = Browser.Post("/permissions", with =>
+            var postResponse = _browser.Post("/permissions", with =>
             {
                 with.HttpRequest();
                 with.JsonBody(new
                 {
                     Grain = "app",
-                    SecurableItem = "permissionprincipal",
+                    SecurableItem = _securableItem,
                     Name = permission
                 });
             }).Result;
 
             // Get by name
-            var getResponse = Browser.Get($"/permissions/app/permissionprincipal/{permission}", with =>
+            var getResponse = _browser.Get($"/permissions/app/{_securableItem}/{permission}", with =>
             {
                 with.HttpRequest();
             }).Result;
@@ -81,24 +73,24 @@ namespace Fabric.Authorization.IntegrationTests.Modules
         }
 
         [Theory]
-        [DisplayTestMethodName]
-        [InlineData("NewPerm1")]
-        [InlineData("NewPerm2")]
+        [IntegrationTestsFixture.DisplayTestMethodName]
+        [InlineData("B9AEAA5A-C079-4BF8-84AC-44B8EF79B4D1")]
+        [InlineData("3E613C1B-D6A6-491B-8927-91566B5EAF2C")]
         public void TestGetPermission_Success(string permission)
         {
-            var postResponse = Browser.Post("/permissions", with =>
+            var postResponse = _browser.Post("/permissions", with =>
             {
                 with.HttpRequest();
                 with.JsonBody(new
                 {
                     Grain = "app",
-                    SecurableItem = "permissionprincipal",
+                    SecurableItem = _securableItem,
                     Name = permission
                 });
             }).Result;
 
             // Get by name
-            var getResponse = Browser.Get($"/permissions/app/permissionprincipal/{permission}", with =>
+            var getResponse = _browser.Get($"/permissions/app/{_securableItem}/{permission}", with =>
             {
                 with.HttpRequest();
             }).Result;
@@ -108,7 +100,7 @@ namespace Fabric.Authorization.IntegrationTests.Modules
             Assert.Contains(permission, getResponse.Body.AsString());
 
             // Get by secitem
-            getResponse = Browser.Get("/permissions/app/permissionprincipal", with =>
+            getResponse = _browser.Get($"/permissions/app/{_securableItem}", with =>
             {
                 with.HttpRequest();
             }).Result;
@@ -119,35 +111,35 @@ namespace Fabric.Authorization.IntegrationTests.Modules
         }
 
         [Theory]
-        [DisplayTestMethodName]
-        [InlineData("SecItemPerm1")]
-        [InlineData("SecItemPerm2")]
+        [IntegrationTestsFixture.DisplayTestMethodName]
+        [InlineData("AE01ADF0-B88F-46A5-BBA0-3C697B0386C3")]
+        [InlineData("807D63CD-4AB3-4756-ADED-3EA4B6C960FE")]
         public void TestGetPermissionForSecItem_Success(string permission)
         {
-            var postResponse = Browser.Post("/permissions", with =>
+            var postResponse = _browser.Post("/permissions", with =>
             {
                 with.HttpRequest();
                 with.JsonBody(new
                 {
                     Grain = "app",
-                    SecurableItem = "permissionprincipal",
+                    SecurableItem = _securableItem,
                     Name = permission + "_1"
                 });
             }).Result;
 
-            postResponse = Browser.Post("/permissions", with =>
+            postResponse = _browser.Post("/permissions", with =>
             {
                 with.HttpRequest();
                 with.JsonBody(new
                 {
                     Grain = "app",
-                    SecurableItem = "permissionprincipal",
+                    SecurableItem = _securableItem,
                     Name = permission + "_2"
                 });
             }).Result;
 
             // Get by secitem
-            var getResponse = Browser.Get("/permissions/app/permissionprincipal", with =>
+            var getResponse = _browser.Get($"/permissions/app/{_securableItem}", with =>
             {
                 with.HttpRequest();
             }).Result;
@@ -160,18 +152,18 @@ namespace Fabric.Authorization.IntegrationTests.Modules
         }
 
         [Theory]
-        [DisplayTestMethodName]
-        [InlineData("RepeatedPermission1")]
-        [InlineData("RepeatedPermission2")]
+        [IntegrationTestsFixture.DisplayTestMethodName]
+        [InlineData("CA75CDD1-51F9-40C3-94C7-6DE9EA87FEEC")]
+        [InlineData("3A8AA05D-B2E8-4E45-94CA-E8E485356999")]
         public void TestAddNewPermission_Fail(string permission)
         {
-            var validPostResponse = Browser.Post("/permissions", with =>
+            var validPostResponse = _browser.Post("/permissions", with =>
             {
                 with.HttpRequest();
                 with.JsonBody(new
                 {
                     Grain = "app",
-                    SecurableItem = "permissionprincipal",
+                    SecurableItem = _securableItem,
                     Name = permission
                 });
             }).Result;
@@ -179,13 +171,13 @@ namespace Fabric.Authorization.IntegrationTests.Modules
             Assert.Equal(HttpStatusCode.Created, validPostResponse.StatusCode);
 
             // Repeat
-            var postResponse = Browser.Post("/permissions", with =>
+            var postResponse = _browser.Post("/permissions", with =>
             {
                 with.HttpRequest();
                 with.JsonBody(new
                 {
                     Grain = "app",
-                    SecurableItem = "permissionprincipal",
+                    SecurableItem = _securableItem,
                     Name = permission
                 });
             }).Result;
@@ -194,26 +186,26 @@ namespace Fabric.Authorization.IntegrationTests.Modules
         }
 
         [Theory]
-        [DisplayTestMethodName]
-        [InlineData("PermissionToBeDeleted")]
-        [InlineData("PermissionToBeDeleted2")]
+        [IntegrationTestsFixture.DisplayTestMethodName]
+        [InlineData("2D8AA649-EEE3-4B96-8A9C-DB24936CFBEF")]
+        [InlineData("537B1742-4D2B-42F4-A29B-73DA3E074E93")]
         public void TestDeletePermission_Success(string permission)
         {
             var id = Guid.NewGuid();
 
-            Browser.Post("/permissions", with =>
+            _browser.Post("/permissions", with =>
             {
                 with.HttpRequest();
                 with.JsonBody(new
                 {
                     Id = id.ToString(),
                     Grain = "app",
-                    SecurableItem = "permissionprincipal",
+                    SecurableItem = _securableItem,
                     Name = permission
                 });
             }).Wait();
 
-            var delete = Browser.Delete($"/permissions/{id}", with =>
+            var delete = _browser.Delete($"/permissions/{id}", with =>
             {
                 with.HttpRequest();
             }).Result;
@@ -222,12 +214,12 @@ namespace Fabric.Authorization.IntegrationTests.Modules
         }
 
         [Theory]
-        [DisplayTestMethodName]
+        [IntegrationTestsFixture.DisplayTestMethodName]
         [InlineData("18F06565-AA9E-4315-AF27-CEFC165B20FA")]
         [InlineData("18F06565-AA9E-4315-AF27-CEFC165B20FB")]
         public void TestDeletePermission_Fail(string permission)
         {
-            var delete = Browser.Delete($"/permissions/{permission}", with =>
+            var delete = _browser.Delete($"/permissions/{permission}", with =>
             {
                 with.HttpRequest();
             }).Result;
