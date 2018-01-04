2266b3d55ff34feed774642023e5fc86db30aab0
Brian Smith <brian.smith@healthcatalyst.com>
2018-01-04 12:44:55 -0500

    implement SqlServer store registration

--- Fabric.Authorization.API/Startup.cs
+++ Fabric.Authorization.API/Startup.cs
@@ -43,7 +43,6 @@ namespace Fabric.Authorization.API
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            
             services.AddWebEncoders();
         }
 
