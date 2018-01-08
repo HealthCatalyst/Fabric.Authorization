f77b119dc1f7a3cd59044b7d4dc2f1fd9012cbeb
Brian Smith <brian.smith@healthcatalyst.com>
2018-01-08 12:33:03 -0500

    moved DB-specific registrations to their own classes

---
+++ Fabric.Authorization.API/DependencyInjection/InMemoryConfigurator.cs
@@ -0,0 +1,26 @@
+﻿using Fabric.Authorization.Domain.Services;
+using Fabric.Authorization.Domain.Stores;
+using Fabric.Authorization.Persistence.InMemory.Services;
+using Fabric.Authorization.Persistence.InMemory.Stores;
+using Nancy.TinyIoc;
+
+namespace Fabric.Authorization.API.DependencyInjection
+{
+    public class InMemoryConfigurator : IPersistenceConfigurator
+    {
+        public void ConfigureSingletons(TinyIoCContainer container)
+        {
+            container.Register<IDbBootstrapper, InMemoryDbBootstrapper>();
+            container.Register<IIdentifierFormatter, IdpIdentifierFormatter>();
+            container.Register<IRoleStore, InMemoryRoleStore>();
+            container.Register<IUserStore, InMemoryUserStore>();
+            container.Register<IPermissionStore, InMemoryPermissionStore>();
+            container.Register<IGroupStore, InMemoryGroupStore>();
+            container.Register<IClientStore, InMemoryClientStore>();
+        }
+
+        public void ConfigureRequestInstances(TinyIoCContainer container)
+        {
+        }
+    }
+}
--- Fabric.Authorization.API/Extensions/TinyIocExtensions.cs
+++ Fabric.Authorization.API/Extensions/TinyIocExtensions.cs
@@ -1,18 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using Fabric.Authorization.API.Configuration;
 using Fabric.Authorization.API.RemoteServices.Identity.Providers;
 using Fabric.Authorization.API.Services;
 using Fabric.Authorization.Domain.Resolvers.Permissions;
 using Fabric.Authorization.Domain.Services;
-using Fabric.Authorization.Domain.Stores;
-using Fabric.Authorization.Persistence.CouchDb.Services;
-using Fabric.Authorization.Persistence.CouchDb.Stores;
-using Fabric.Authorization.Persistence.InMemory.Stores;
-using Fabric.Authorization.Persistence.SqlServer.Services;
-using Fabric.Authorization.Persistence.SqlServer.Stores;
 using Nancy.TinyIoc;
-using Serilog.Core;
 
 namespace Fabric.Authorization.API.Extensions
 {
@@ -35,60 +27,8 @@ namespace Fabric.Authorization.API.Extensions
                 typeof(RolePermissionResolverService)
             });
 
-            return container;
-        }
-
-        public static TinyIoCContainer RegisterInMemoryStores(this TinyIoCContainer container)
-        {
-            container.Register<IIdentifierFormatter, IdpIdentifierFormatter>();
-            container.Register<IRoleStore, InMemoryRoleStore>();
-            container.Register<IUserStore, InMemoryUserStore>();
-            container.Register<IPermissionStore, InMemoryPermissionStore>();
-            container.Register<IGroupStore, InMemoryGroupStore>();
-            container.Register<IClientStore, InMemoryClientStore>();
-
-            return container;
-        }
-
-        public static TinyIoCContainer RegisterCouchDbStores(this TinyIoCContainer container,
-            IAppConfiguration appConfiguration, LoggingLevelSwitch levelSwitch)
-        {
             container.Register<IEventService, EventService>();
             container.Register<IEventContextResolverService, EventContextResolverService>();
-            container.Register<IDocumentDbService>(
-                (c, p) => c.Resolve<AuditingDocumentDbService>(new NamedParameterOverloads
-                {
-                    {"innerDocumentDbService", c.Resolve<IDocumentDbService>("inner")}
-                }), "auditing");
-            container.Register<IDocumentDbService>(
-                (c, p) => c.Resolve<CachingDocumentDbService>(new NamedParameterOverloads
-                {
-                    {"innerDocumentDbService", c.Resolve<IDocumentDbService>("auditing")}
-                }));
-
-            // TODO: if other CouchDB store types need a different formatter, we'll have to register those
-            container.Register<IIdentifierFormatter, IdpIdentifierFormatter>();
-
-            container.Register<IRoleStore, CouchDbRoleStore>();
-            container.Register<IUserStore, CouchDbUserStore>();
-            container.Register<IPermissionStore, CouchDbPermissionStore>();
-            container.Register<IGroupStore, CouchDbGroupStore>();
-            container.Register<IClientStore, CouchDbClientStore>();
-
-            return container;
-        }
-
-        public static TinyIoCContainer RegisterSqlServerStores(
-            this TinyIoCContainer container,
-            IAppConfiguration appConfiguration,
-            LoggingLevelSwitch levelSwitch)
-        {
-            container.Register<IAuthorizationDbContext, AuthorizationDbContext>();
-            container.Register<IRoleStore, SqlServerRoleStore>();
-            container.Register<IUserStore, SqlServerUserStore>();
-            container.Register<IPermissionStore, SqlServerPermissionStore>();
-            container.Register<IGroupStore, SqlServerGroupStore>();
-            container.Register<IClientStore, SqlServerClientStore>();
 
             return container;
         }
