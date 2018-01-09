6f6e8d7996d657e27aed5f0ac4fa79a5150728fd
Kyle Paul <djjoebanks@gmail.com>
2018-01-09 12:13:33 -0800

    only initialize automapper profiles once. comment out sql server client tests

--- Fabric.Authorization.Persistence.SqlServer/Services/SqlServerDbBootstrapper.cs
+++ Fabric.Authorization.Persistence.SqlServer/Services/SqlServerDbBootstrapper.cs
@@ -6,10 +6,15 @@ namespace Fabric.Authorization.Persistence.SqlServer.Services
 {
     public class SqlServerDbBootstrapper : IDbBootstrapper
     {
+        static SqlServerDbBootstrapper()
+        {
+            //register all the automapper profiles - only ever want to call this once
+            Mapper.Initialize(cfg => cfg.AddProfiles(typeof(ClientMapperProfile)));
+        }
+
         public void Setup()
         {
-            //register all the automapper profiles
-            Mapper.Initialize(cfg => cfg.AddProfiles(typeof(ClientMapperProfile)));
+            
         }
     }
 }
