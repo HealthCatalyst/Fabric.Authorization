47b59ed61884784dc6dfbc25846081495bd061a8
michael.vidal <michael.vidal@healthcatalyst.com>
2018-01-09 10:43:07 -0500

    Switched database name generation to use GUIDs instead of ticks to deal with concurrent database creation. Cleaned up group tests a little.

--- Fabric.Authorization.IntegrationTests/IntegrationTestsFixture.cs
+++ Fabric.Authorization.IntegrationTests/IntegrationTestsFixture.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Data.SqlClient;
+using System.IO;
 using System.Reflection;
 using System.Security.Claims;
 using Fabric.Authorization.API.Configuration;
@@ -15,7 +17,6 @@ using Fabric.Authorization.Persistence.SqlServer.Services;
 using Fabric.Platform.Shared.Configuration;
 using IdentityModel;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Caching.Memory;
 using Moq;
@@ -31,11 +32,13 @@ namespace Fabric.Authorization.IntegrationTests
     {
         protected CouchDbSettings CouchDbSettings { get; }
         protected ConnectionStrings ConnectionStrings { get; }
+        private string DatabaseNameSuffix { get; }
         public IntegrationTestsFixture()
         {
             CouchDbSettings = GetCouchDbSettings();
-            ConnectionStrings = GetSqlServerConnection();
-            //CreateSqlServerDb();
+            DatabaseNameSuffix = GetDatabaseNameSuffix();
+            ConnectionStrings = GetSqlServerConnection(DatabaseNameSuffix);
+            //CreateSqlServerDatabase();
         }
         public Browser Browser { get; set; }
 
@@ -64,7 +67,7 @@ namespace Fabric.Authorization.IntegrationTests
                 }
             };
             var hostingEnvironment = new Mock<IHostingEnvironment>();
-            
+
             var bootstrapper = new TestBootstrapper(new Mock<ILogger>().Object, appConfiguration,
                 new LoggingLevelSwitch(), hostingEnvironment.Object, principal, identityServiceProvider);
 
@@ -118,15 +121,15 @@ namespace Fabric.Authorization.IntegrationTests
         protected AuthorizationDbContext AuthorizationDbContext
         {
             get
-            {         
+            {
                 var builder = new DbContextOptionsBuilder<AuthorizationDbContext>();
 
                 builder.UseSqlServer(ConnectionStrings.AuthorizationDatabase);
 
                 var testIdentity = new ClaimsIdentity();
                 testIdentity.AddClaim(new Claim(JwtClaimTypes.ClientId, "testing"));
-                
-                var nancyContext = new NancyContext {CurrentUser = new ClaimsPrincipal(testIdentity)};
+
+                var nancyContext = new NancyContext { CurrentUser = new ClaimsPrincipal(testIdentity) };
                 var nancyContextWrapper = new NancyContextWrapper(nancyContext);
 
                 return new AuthorizationDbContext(new EventContextResolverService(nancyContextWrapper), ConnectionStrings);
@@ -164,16 +167,16 @@ namespace Fabric.Authorization.IntegrationTests
             return config;
         }
 
-        private long GetDatabaseNameSuffix()
+        private string GetDatabaseNameSuffix()
         {
-            return DateTime.UtcNow.Ticks;
+            var id = Guid.NewGuid().ToString().Replace("-", "");
+            return id;
         }
 
-        private ConnectionStrings GetSqlServerConnection()
+        private ConnectionStrings GetSqlServerConnection(string databaseNameSuffix)
         {
             var sqlServerHost = Environment.GetEnvironmentVariable(SqlServerEnvironmentVariable) ?? ".";
             var sqlServerSecurityString = GetSqlServerSecurityString();
-            var databaseNameSuffix = GetDatabaseNameSuffix();
             var connectionString = new ConnectionStrings
             {
                 AuthorizationDatabase = $"Server={sqlServerHost};Database=Authorization-{databaseNameSuffix};{sqlServerSecurityString};MultipleActiveResultSets=true"
@@ -192,6 +195,67 @@ namespace Fabric.Authorization.IntegrationTests
                 securityString = $"User Id={sqlServerUserName};Password={sqlServerPassword}";
             }
             return securityString;
+        }
+
+        private void CreateSqlServerDatabase()
+        {
+            var targetDbName = $"Authorization-{DatabaseNameSuffix}";
+            var connection =
+                ConnectionStrings.AuthorizationDatabase.Replace(targetDbName, "master");
+            var file = new FileInfo("Fabric.Authorization.SqlServer_Create.sql");
+
+            var createDbScript = file.OpenText().ReadToEnd()
+                .Replace("$(DatabaseName)", targetDbName);
+
+            var splitter = new[] { "GO\r\n" };
+            var commandTexts = createDbScript.Split(splitter, StringSplitOptions.RemoveEmptyEntries);
+            int x;
+            using (var conn = new SqlConnection(connection))
+            {
+                conn.Open();
+                using (var command = new SqlCommand("query", conn))
+                {
+                    for (x = 0; x < commandTexts.Length; x++)
+                    {
+                        var commandText = commandTexts[x];
+
+                        // break if we just created the Identity DB
+                        if (commandText.StartsWith("CREATE DATABASE"))
+                        {
+                            var commandParts = commandText.Split(
+                                new[] { " ON " },
+                                StringSplitOptions.RemoveEmptyEntries);
+
+                            command.CommandText = commandParts[0];
+                            command.ExecuteNonQuery();
+                            break;
+                        }
+                    }
+                }
+            }
+
+            // establish a connection to the newly created Identity DB
+            using (var conn = new SqlConnection(ConnectionStrings.AuthorizationDatabase))
+            {
+                conn.Open();
+
+                using (var command = new SqlCommand("query", conn))
+                {
+                    for (x = x + 1; x < commandTexts.Length; x++)
+                    {
+                        var commandText = commandTexts[x];
+
+                        // skip generated SqlPackage commands and comments
+                        if (commandText.StartsWith(":") || commandText.StartsWith("/*"))
+                        {
+                            continue;
+                        }
+
+                        command.CommandText = commandText.Replace("HCFabricAuthorizationData1", "PRIMARY").Replace("HCFabricAuthorizationIndex1", "PRIMARY").TrimEnd(Environment.NewLine.ToCharArray());
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
         }
 
         #region IDisposable implementation
