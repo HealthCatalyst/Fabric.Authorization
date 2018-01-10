646cfd318afd2a722ac2f7f763d5b77b9fa527b2
Brian Smith <brian.smith@healthcatalyst.com>
2018-01-10 13:07:54 -0500

    account for null or empty grain filter when querying roles

--- Fabric.Authorization.Persistence.SqlServer/Stores/SqlServerRoleStore.cs
+++ Fabric.Authorization.Persistence.SqlServer/Stores/SqlServerRoleStore.cs
@@ -138,8 +138,14 @@ namespace Fabric.Authorization.Persistence.SqlServer.Stores
         {
             var roles = _authorizationDbContext.Roles
                 .Include(r => r.SecurableItem)
-                .Where(r => string.Equals(r.Grain, grain, StringComparison.OrdinalIgnoreCase)
-                            && !r.IsDeleted);
+                .Where(r => !r.IsDeleted);
+
+            if (!string.IsNullOrEmpty(grain))
+            {
+                roles = roles
+                    .Where(r => string.Equals(r.Grain, grain, StringComparison.OrdinalIgnoreCase)
+                                && !r.IsDeleted);
+            }
 
             if (!string.IsNullOrWhiteSpace(securableItem))
             {
