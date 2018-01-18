516d7a5ae27aa7558bbaf74fdfcb253478b6b9ca
Kyle Paul <djjoebanks@gmail.com>
2018-01-18 11:59:24 -0800

    only run the sql server client tests

--- Fabric.Authorization.IntegrationTests/SqlServer/SqlServerGroupsTests.cs
+++ Fabric.Authorization.IntegrationTests/SqlServer/SqlServerGroupsTests.cs
@@ -5,9 +5,9 @@ using Xunit;
 namespace Fabric.Authorization.IntegrationTests.SqlServer
 {
     [Collection("SqlServerTests")]
-    public class SqlServerGroupsTests : GroupsTests
+    public class SqlServerGroupsTests //: GroupsTests
     {
-        public SqlServerGroupsTests(IntegrationTestsFixture fixture) : base(fixture, StorageProviders.SqlServer)
+        public SqlServerGroupsTests(IntegrationTestsFixture fixture) //: base(fixture, StorageProviders.SqlServer)
         {
         }
     }
--- Fabric.Authorization.IntegrationTests/SqlServer/SqlServerPermissionsTests.cs
+++ Fabric.Authorization.IntegrationTests/SqlServer/SqlServerPermissionsTests.cs
@@ -5,9 +5,9 @@ using Xunit;
 namespace Fabric.Authorization.IntegrationTests.SqlServer
 {
     [Collection("SqlServerTests")]
-    public class SqlServerPermissionsTests : PermissionsTests
+    public class SqlServerPermissionsTests //: PermissionsTests
     {
-        public SqlServerPermissionsTests(IntegrationTestsFixture fixture) : base(fixture, StorageProviders.SqlServer)
+        public SqlServerPermissionsTests(IntegrationTestsFixture fixture) //: base(fixture, StorageProviders.SqlServer)
         {
         }
     }
