24d26d9313ef285a0207a84a69a427ad0bd01ff5
Kyle Paul <djjoebanks@gmail.com>
2018-01-18 19:00:59 -0800

    change async design

--- Fabric.Authorization.Persistence.CouchDb/Services/CouchDbBootstrapper.cs
+++ Fabric.Authorization.Persistence.CouchDb/Services/CouchDbBootstrapper.cs
@@ -19,20 +19,14 @@ namespace Fabric.Authorization.Persistence.CouchDb.Services
 
         public void Setup()
         {
-            var initializeTask = Task.Run(async () => { await _documentDbService.Initialize(); });
-            initializeTask.Wait();
-            var setupUserTask = Task.Run(async () => { await _documentDbService.SetupDefaultUser(); });
-            setupUserTask.Wait();
-            var addViewsTask = Task.Run(async () =>
-                {
-                    await _documentDbService.AddViews("roles", CouchDbRoleStore.GetViews());
-                });
-            addViewsTask.Wait();
-            var addPermissionViewsTask = Task.Run(async () =>
-                {
-                    await _documentDbService.AddViews("permissions", CouchDbPermissionStore.GetViews());
-                });
-            addPermissionViewsTask.Wait();
+            var initializeTask = Task.Run(async () =>
+            {
+                await _documentDbService.Initialize();
+                await _documentDbService.SetupDefaultUser();
+                await _documentDbService.AddViews("roles", CouchDbRoleStore.GetViews());
+                await _documentDbService.AddViews("permissions", CouchDbPermissionStore.GetViews());
+            });
+            initializeTask.Wait();            
         }
     }
 }
