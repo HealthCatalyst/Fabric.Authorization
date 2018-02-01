7443d159004e5a668506b0cd9383507644180e51
michael.vidal <michael.vidal@healthcatalyst.com>
2018-02-01 06:50:10 -0500

    Removed unnecessary sleep from script.

--- Fabric.Authorization.API/scripts/run-integration-tests.sh
+++ Fabric.Authorization.API/scripts/run-integration-tests.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-sleep 3
 "/C/Program Files (x86)/Microsoft Visual Studio/2017/Professional/MSBuild/15.0/Bin/MSBuild.exe" ../../Fabric.Authorization.SqlServer/Fabric.Authorization.SqlServer.sqlproj
 cp ../../Fabric.Authorization.SqlServer/bin/Debug/Fabric.Authorization.SqlServer_Create.sql ../../Fabric.Authorization.IntegrationTests/bin/Debug/netcoreapp1.1/Fabric.Authorization.SqlServer_Create.sql
 
