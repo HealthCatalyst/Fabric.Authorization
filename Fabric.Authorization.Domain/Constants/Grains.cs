c26e44f9206f8164112cb88c2440b636573f9cc1
michael.vidal <michael.vidal@healthcatalyst.com>
2018-02-02 11:05:49 -0500

    Updated SqlServerDbBootstrapper to add built in grains at startup.

---
+++ Fabric.Authorization.Domain/Constants/Grains.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Fabric.Authorization.Domain.Models;
+
+namespace Fabric.Authorization.Domain.Constants
+{
+    public static class Grains
+    {
+        public static IList<Grain> BuiltInGrains = new List<Grain>
+        {
+            new Grain
+            {
+                Name = "app"
+            },
+            new Grain
+            {
+                Name = "dos",
+                IsShared = true,
+                RequiredWriteScopes = new List<string> { "fabric/authorization.dos.write" }
+            }
+        };
+    }
+}
---
+++ Fabric.Authorization.Persistence.SqlServer/Services/NoOpEventContextResolverService.cs
@@ -0,0 +1,12 @@
+﻿using Fabric.Authorization.Domain.Services;
+
+namespace Fabric.Authorization.Persistence.SqlServer.Services
+{
+    public class NoOpEventContextResolverService : IEventContextResolverService
+    {
+        public string Username { get; set; }
+        public string ClientId { get; set; }
+        public string Subject { get; set; }
+        public string RemoteIpAddress { get; set; }
+    }
+}
