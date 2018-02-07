414a6c467f47973cd09f8648946b75e74e4592f3
michael.vidal <michael.vidal@healthcatalyst.com>
2018-02-06 23:09:27 -0500

    Added a CouchDbGrainStore so the CouchDb version doesn't blow up.

---
+++ Fabric.Authorization.Persistence.CouchDb/Stores/CouchDbGrainStore.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Fabric.Authorization.Domain.Models;
+using Fabric.Authorization.Domain.Stores;
+
+namespace Fabric.Authorization.Persistence.CouchDb.Stores
+{
+    public class CouchDbGrainStore : IGrainStore
+    {
+        public Task<Grain> Get(string name)
+        {
+            //  this allows for backward compatibility with CouchDb backed versions.
+            //  this implementation will be removed once we convert clients over to the sql server backed version.
+            return Task.FromResult(default(Grain));
+        }
+    }
+}
