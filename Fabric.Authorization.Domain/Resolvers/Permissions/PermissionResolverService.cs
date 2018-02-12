1749c89b7fc14d868b861b1b65a0d9cfc00a38a7
Brian Smith <brian.smith@healthcatalyst.com>
2018-02-12 15:03:40 -0500

    removed unused dependencies

--- Fabric.Authorization.Domain/Resolvers/Permissions/PermissionResolverService.cs
+++ Fabric.Authorization.Domain/Resolvers/Permissions/PermissionResolverService.cs
@@ -1,26 +1,19 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using Fabric.Authorization.Domain.Resolvers.Models;
-using Fabric.Authorization.Domain.Services;
 using Serilog;
 
 namespace Fabric.Authorization.Domain.Resolvers.Permissions
 {
     public class PermissionResolverService : IPermissionResolverService
     {
-        protected readonly PermissionService PermissionService;
-        protected readonly RoleService RoleService;
         protected readonly ILogger Logger;
         protected readonly IEnumerable<IPermissionResolverService> PermissionResolverServices;
 
         public PermissionResolverService(
-            RoleService roleService,
-            PermissionService permissionService,
             IEnumerable<IPermissionResolverService> permissionResolverServices,
             ILogger logger)
         {
-            RoleService = roleService;
-            PermissionService = permissionService;
             Logger = logger;
             PermissionResolverServices = permissionResolverServices;
         }
