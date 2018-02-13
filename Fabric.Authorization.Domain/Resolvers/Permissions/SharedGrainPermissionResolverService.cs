543fbc92710b3d69335daa9736fda4994ccedaaa
Brian Smith <brian.smith@healthcatalyst.com>
2018-02-13 11:00:52 -0500

    added resolver service that resolves permissions tied to shared grains

---
+++ Fabric.Authorization.Domain/Resolvers/Permissions/SharedGrainPermissionResolverService.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Fabric.Authorization.Domain.Resolvers.Models;
+using Fabric.Authorization.Domain.Services;
+
+namespace Fabric.Authorization.Domain.Resolvers.Permissions
+{
+    public class SharedGrainPermissionResolverService : IPermissionResolverService
+    {
+        private readonly GrainService _grainService;
+        private readonly IEnumerable<IPermissionResolverService> _permissionResolverServices;
+
+        public SharedGrainPermissionResolverService(GrainService grainService, IEnumerable<IPermissionResolverService> permissionResolverServices)
+        {
+            _grainService = grainService ?? throw new ArgumentNullException(nameof(grainService));
+            _permissionResolverServices = permissionResolverServices ?? throw new ArgumentNullException(nameof(permissionResolverServices));
+        }
+
+        public async Task<PermissionResolutionResult> Resolve(PermissionResolutionRequest resolutionRequest)
+        {
+            /*if (!resolutionRequest.IncludeSharedPermissions)
+            {
+                return new PermissionResolutionResult();
+            }*/
+
+            var sharedGrains = await _grainService.GetSharedGrains();
+
+            var permissionResolutionResult = new PermissionResolutionResult();
+            foreach (var sharedGrain in sharedGrains)
+            {
+                foreach (var securableItem in sharedGrain.SecurableItems)
+                {
+                    foreach (var permissionResolverService in _permissionResolverServices)
+                    {
+                        if (permissionResolverService.GetType() == GetType())
+                        {
+                            continue;
+                        }
+                        var result = await permissionResolverService.Resolve(new PermissionResolutionRequest
+                        {
+                            Grain = sharedGrain.Name,
+                            SecurableItem = securableItem.Name,
+                            IdentityProvider = resolutionRequest.IdentityProvider,
+                            SubjectId = resolutionRequest.SubjectId,
+                            UserGroups = resolutionRequest.UserGroups,
+                            IncludeSharedPermissions = false
+                        });
+
+                        permissionResolutionResult.AllowedPermissions =
+                            permissionResolutionResult.AllowedPermissions.Concat(result.AllowedPermissions);
+
+                        permissionResolutionResult.DeniedPermissions =
+                            permissionResolutionResult.DeniedPermissions.Concat(result.DeniedPermissions);
+                    }
+                }
+            }
+
+            return permissionResolutionResult;
+        }
+    }
+}
