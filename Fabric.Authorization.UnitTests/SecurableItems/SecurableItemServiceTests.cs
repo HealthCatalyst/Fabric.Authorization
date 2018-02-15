03674a23c4f6a867878706782354cc2a430c0d60
Brian Smith <brian.smith@healthcatalyst.com>
2018-02-14 21:36:13 -0500

    removed unused using statements

--- Fabric.Authorization.UnitTests/SecurableItems/SecurableItemServiceTests.cs
+++ Fabric.Authorization.UnitTests/SecurableItems/SecurableItemServiceTests.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 using Fabric.Authorization.Domain.Models;
 using Fabric.Authorization.Domain.Services;
 using Fabric.Authorization.Domain.Stores;
