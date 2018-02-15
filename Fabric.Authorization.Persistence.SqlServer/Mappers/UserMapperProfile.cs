4e17537d7e648426854085838965aa56eaa01dfd
michael.vidal <michael.vidal@healthcatalyst.com>
2018-02-15 06:23:58 -0500

    Updated user and role mappers.

--- Fabric.Authorization.Persistence.SqlServer/Mappers/UserMapperProfile.cs
+++ Fabric.Authorization.Persistence.SqlServer/Mappers/UserMapperProfile.cs
@@ -11,7 +11,8 @@ namespace Fabric.Authorization.Persistence.SqlServer.Mappers
             CreateMap<EntityModels.User, Domain.Models.User>()
                 .ForMember(x => x.Id, opt => opt.MapFrom(src => new UserIdentifierFormatter().Format(src)))
                 .ForMember(x => x.Groups, opt => opt.MapFrom(src => src.GroupUsers.Select(gu => gu.Group.Name)))
-                .ForMember(x => x.Permissions, opt => opt.MapFrom(src => src.UserPermissions.Select(up => up.Permission)))               
+                .ForMember(x => x.Permissions, opt => opt.MapFrom(src => src.UserPermissions.Select(up => up.Permission)))
+                .ForMember(x => x.Roles, opt => opt.MapFrom(src => src.RoleUsers.Select(ru => ru.Role)))
                 .ReverseMap()
                 .ForMember(x => x.Id, opt => opt.Ignore());
         }
