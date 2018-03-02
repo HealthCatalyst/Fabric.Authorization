b05a03f507ab112d8cdf1250258012a4fb14c5ef
Kyle Paul <djjoebanks@gmail.com>
2018-03-02 12:50:16 -0500

    remove test components and add real ones

--- Fabric.Authorization.AccessControl/src/app/modules/access-control/access-control.module.ts
+++ Fabric.Authorization.AccessControl/src/app/modules/access-control/access-control.module.ts
@@ -1,23 +1,23 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AccessControlRoutingModule } from './access-control-routing.module';
-import { UserlistComponent } from './userlist/userlist.component';
-import { UseraddComponent } from './useradd/useradd.component';
-import { HttpClientModule } from '@angular/common/http';
 import { FabricAuthBaseService } from '../../services/fabric-auth-base.service';
 import { FabricAuthGroupService } from '../../services/fabric-auth-group.service';
 import { FabricAuthMemberSearchService } from '../../services/fabric-auth-member-search.service';
 import { FabricAuthUserService } from '../../services/fabric-auth-user.service';
 import { FabricExternalIdpSearchService } from '../../services/fabric-external-idp-search.service';
 import { httpInterceptorProviders } from '../../services/interceptors';
+import { MemberListComponent } from './member-list/member-list.component';
+import { MemberAddComponent } from './member-add/member-add.component';
 
 @NgModule({
   imports: [
     CommonModule,
     AccessControlRoutingModule
   ],
-  declarations: [UserlistComponent, UseraddComponent],  
+  declarations: [MemberListComponent, MemberAddComponent],  
   providers: [
     FabricAuthBaseService,
     FabricAuthGroupService,
@@ -26,8 +26,8 @@ import { httpInterceptorProviders } from '../../services/interceptors';
     FabricExternalIdpSearchService
   ],
   exports:[
-    UserlistComponent,
-    UseraddComponent
+    MemberListComponent,
+    MemberAddComponent
   ]
 })
 export class AccessControlModule { }
---
+++ Fabric.Authorization.AccessControl/src/app/modules/access-control/member-add/member-add.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-member-add',
+  templateUrl: './member-add.component.html',
+  styleUrls: ['./member-add.component.css']
+})
+export class MemberAddComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
