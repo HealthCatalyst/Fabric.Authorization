9cc9ff1fe24e08c01ca52ae13a97dbdf59cc6920
Brian Smith <brian.smith@healthcatalyst.com>
2018-03-05 14:01:04 -0500

    pass in query string parameters to member search request

--- Fabric.Authorization.AccessControl/src/app/services/fabric-auth-member-search.service.ts
+++ Fabric.Authorization.AccessControl/src/app/services/fabric-auth-member-search.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Response } from "@angular/http";
 import { Observable } from 'rxjs/Rx';
@@ -22,8 +22,32 @@ export class FabricAuthMemberSearchService extends FabricAuthBaseService {
   }
 
   public searchMembers(request: AuthMemberSearchRequest) : Observable<AuthMemberSearchResult[]> {
+
+    let params = new HttpParams()
+      .set('clientId', request.clientId);
+
+    if (request.pageSize) {
+      params.set('pageSize', request.pageSize.toString());
+    }
+
+    if (request.pageNumber) {
+      params.set('pageNumber', request.pageNumber.toString());
+    }
+
+    if (request.sortKey) {
+      params.set('sortKey', request.sortKey);
+    }
+
+    if (request.sortDirection) {
+      params.set('sortDirection', request.sortDirection);
+    }
+
+    if (request.filter) {
+      params.set('filter', request.filter);
+    }
+
     return this.httpClient
-      .get(FabricAuthMemberSearchService.baseMemberApiUrl)
+      .get(FabricAuthMemberSearchService.baseMemberApiUrl, {params})
       .map((response: Response) => {
         this.handleError(response);
         return response.json();
