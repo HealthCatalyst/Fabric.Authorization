e506e119fb733cb7eb87c0b705d173dad49d18d5
Brian Smith <brian.smith@healthcatalyst.com>
2018-03-05 08:55:40 -0500

    added HttpClientModule import so that HttpClient is available to the application

--- Fabric.Authorization.AccessControl/src/app/app.module.ts
+++ Fabric.Authorization.AccessControl/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule  
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
