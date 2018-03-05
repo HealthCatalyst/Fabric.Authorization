cd6ffb7aa7890209afaad84777448684eb29a2ea
Kyle Paul <djjoebanks@gmail.com>
2018-03-05 14:30:01 -0500

    added a couple components to the main app. updated auth service

---
+++ Fabric.Authorization.AccessControl/src/app/login/login.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/global/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor(private authService: AuthService) { 
+    this.authService.login();
+  }
+
+  ngOnInit() {
+  }
+
+}
