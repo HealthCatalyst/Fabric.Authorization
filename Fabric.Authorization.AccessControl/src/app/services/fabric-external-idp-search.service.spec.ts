5fde01857f27de692481a12c130a8ee00d518120
Brian Smith <brian.smith@healthcatalyst.com>
2018-03-05 15:48:16 -0500

    set up http mocks for each service test

--- Fabric.Authorization.AccessControl/src/app/services/fabric-external-idp-search.service.spec.ts
+++ Fabric.Authorization.AccessControl/src/app/services/fabric-external-idp-search.service.spec.ts
@@ -1,12 +1,21 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed, inject } from '@angular/core/testing';
 
 import { FabricExternalIdpSearchService } from './fabric-external-idp-search.service';
 
 describe('FabricExternalIdpSearchService', () => {
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ],
       providers: [FabricExternalIdpSearchService]
     });
+
+    httpClient = TestBed.get(HttpClient);
+    httpTestingController = TestBed.get(HttpTestingController);
   });
 
   it('should be created', inject([FabricExternalIdpSearchService], (service: FabricExternalIdpSearchService) => {
