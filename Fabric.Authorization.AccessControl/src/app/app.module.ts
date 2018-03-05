fe2ab2e2f06c4d0a4a31cfaa79941e690332fdd0
Kyle Paul <djjoebanks@gmail.com>
2018-03-05 16:18:17 -0500

    wired up authenticating with identity server

--- Fabric.Authorization.AccessControl/src/app/app.module.ts
+++ Fabric.Authorization.AccessControl/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
-
-import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
-import { httpInterceptorProviders } from './services/interceptors';
 
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { httpInterceptorProviders } from './services/interceptors';
 import { AuthService } from '../app/services/global/auth.service';
+
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 
