0ded0adb0502458bcf57b9d0225d0a7d5df70409
Brian Smith <brian.smith@healthcatalyst.com>
2018-03-06 16:56:01 -0500

    fixed issue with test

--- Fabric.Authorization.AccessControl/src/app/services/fabric-auth-group.service.spec.ts
+++ Fabric.Authorization.AccessControl/src/app/services/fabric-auth-group.service.spec.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpErrorResponse, HttpRequest } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { Pipe, PipeTransform } from '@angular/core';
 import { TestBed, inject, async } from '@angular/core/testing';
@@ -20,31 +20,49 @@ describe('FabricAuthGroupService', () => {
     expect(service).toBeTruthy();
   }));
 
-  fit('should deserialize all group proper',
+  fit('addUserToCustomGroup should deserialize all properties',
     async(  
       inject([HttpClient, HttpTestingController, FabricAuthGroupService], (
         httpClient: HttpClient,
         httpTestingController: HttpTestingController,
         service: FabricAuthGroupService) => {
 
-        let group = new Group('Group 1', 'Custom');
+        const mockResponse = {
+          groupName: 'Group 1',
+          groupSource: 'Custom',
+          users: [
+            {
+              name: 'First Last',
+              subjectId: 'Sub123',
+              identityProvider: 'Windows'
+            }
+          ]
+        };
+
         let user = new User('Windows', 'Sub123');
         user.name = 'First Last';
-        group.users.push(user);
 
-        service.addUserToCustomGroup(group.groupName, user).subscribe(returnedGroup => {
-          console.log(returnedGroup);
-          expect(returnedGroup.groupName).toBe(group.groupName);
-          expect(returnedGroup.groupSource).toBe(group.groupSource);
+        // set up subscription to POST '/groups/Group 1/user' and service response expectations
+        service.addUserToCustomGroup(mockResponse.groupName, user).subscribe(returnedGroup => {
+          expect(returnedGroup.groupName).toBe(mockResponse.groupName);
+          expect(returnedGroup.groupSource).toBe(mockResponse.groupSource);
           expect(returnedGroup.users).toBeDefined();
           expect(returnedGroup.users.length).toEqual(1);
-          expect(returnedGroup.users[0]).toBe(user);
-          expect(returnedGroup.users[0]).toEqual(user);
+          let returnedUser = returnedGroup.users[0];
+          let originalUser = mockResponse.users[0];
+          expect(returnedUser.subjectId).toEqual(originalUser.subjectId);
+          expect(returnedUser.identityProvider).toEqual(originalUser.identityProvider);
+          expect(returnedUser.name).toEqual(originalUser.name);
         });
 
-        const req = httpTestingController.expectOne(`${FabricAuthGroupService.baseGroupApiUrl}/${group.groupName}/users`);
+        // simulate response from POST '/groups/Group 1/user' and flush response
+        // to trigger subscription above
+        const req = httpTestingController.expectOne(`${FabricAuthGroupService.baseGroupApiUrl}/${mockResponse.groupName}/users`);
         expect(req.request.method).toBe("POST");
-        req.flush(JSON.stringify(group));
+        console.log(mockResponse);
+        req.flush(mockResponse, {status: 201, statusText: 'Created'});
+        
+        httpTestingController.verify();
       })
     )
   );
