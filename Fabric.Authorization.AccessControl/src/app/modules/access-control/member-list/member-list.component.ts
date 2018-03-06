ee162957df6c360d053c257a1f2be287efd1daf3
Kyle Paul <djjoebanks@gmail.com>
2018-03-06 14:19:27 -0500

    add a basic table to member-list and bind the members to it

--- Fabric.Authorization.AccessControl/src/app/modules/access-control/member-list/member-list.component.ts
+++ Fabric.Authorization.AccessControl/src/app/modules/access-control/member-list/member-list.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
+
+import { FabricAuthMemberSearchService } from '../../../services/fabric-auth-member-search.service';
+import { AuthMemberSearchRequest } from '../../../models/authMemberSearchRequest';
+import { AuthMemberSearchResult } from '../../../models/authMemberSearchResult';
 
 @Component({
   selector: 'app-member-list',
@@ -7,9 +12,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MemberListComponent implements OnInit {
 
-  constructor() { }
+  members: AuthMemberSearchResult[];
+
+  constructor(private memberSearchService: FabricAuthMemberSearchService) { }
 
   ngOnInit() {
+    this.getMembers();
+  }
+
+  getMembers() {
+    var self = this;
+    var searchRequest = new AuthMemberSearchRequest();
+    searchRequest.clientId = 'fabric-angularsample';
+
+    return this.memberSearchService.searchMembers(searchRequest)
+    .subscribe(function(memberList){
+      self.members = memberList;
+    });
   }
 
 }
