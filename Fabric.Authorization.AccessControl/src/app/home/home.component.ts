24764b9d012db26672300aadc3b1192f0870a303
Kyle Paul <djjoebanks@gmail.com>
2018-03-06 20:51:24 -0500

    fixed up things to get the member-list component loading members

--- Fabric.Authorization.AccessControl/src/app/home/home.component.ts
+++ Fabric.Authorization.AccessControl/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/global/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  profile = {}; 
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.authService.getUser().then(result => {
+      if (result) {
+          this.profile = result.profile;          
+          console.log(JSON.stringify(result.profile));
+          this.authService.getUser().then(function(user){
+            console.log(user.access_token);
+          });
+      }
+    });
   }
 
 }
