fe9c86fb8eadff61bbaf10e8b053a3ae0d9ba4d2
Brian Smith <brian.smith@healthcatalyst.com>
2018-03-08 08:25:19 -0500

    added external IdP search API info to environment files

--- Fabric.Authorization.AccessControl/src/environments/environment.prod.ts
+++ Fabric.Authorization.AccessControl/src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
-  fabricAuthApiUri: "http://localhost:5004",
-  fabricAuthApiVersionSegment: "api/v1"
+  fabricAuthApiUri: "http://localhost:5004/api",
+  fabricAuthApiVersionSegment: "v1",
+  fabricExternalIdPSearchApiUri: "http://localhost:TBD/api",
+  fabricExternalIdPSearchApiVersionSegment: "v1"
 };
