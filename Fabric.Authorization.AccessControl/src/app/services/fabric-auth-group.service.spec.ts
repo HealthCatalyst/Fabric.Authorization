14d2f03cba12c72cfd5402704d21a0e77a11aee5
Brian Smith <brian.smith@healthcatalyst.com>
2018-03-08 16:18:27 -0500

    added remaining group service tests

--- Fabric.Authorization.AccessControl/src/app/services/fabric-auth-group.service.spec.ts
+++ Fabric.Authorization.AccessControl/src/app/services/fabric-auth-group.service.spec.ts
@@ -5,7 +5,7 @@ import { TestBed, inject, async } from '@angular/core/testing';
 import { Observable } from 'rxjs/Rx';
 
 import { FabricAuthGroupService, AccessControlConfigService } from '../services';
-import { Group, User } from '../models';
+import { Group, User, Role } from '../models';
 import { ErrorObservable } from 'rxjs/observable/ErrorObservable';
 import { FabricHttpErrorHandlerInterceptorService } from './interceptors/fabric-http-error-handler-interceptor.service';
 
@@ -66,28 +66,16 @@ fdescribe('FabricAuthGroupService', () => {
         httpTestingController: HttpTestingController,
         service: FabricAuthGroupService) => {
 
-        let user = new User('Windows', 'Sub123');
-        user.name = 'First Last';
-
         // set up subscription to POST '/groups/Group 1/user' and service response expectations
-        service.addUserToCustomGroup(mockGroupUsersResponse.groupName, user).subscribe(returnedGroup => {
-          expect(returnedGroup.groupName).toBe(mockGroupUsersResponse.groupName);
-          expect(returnedGroup.groupSource).toBe(mockGroupUsersResponse.groupSource);
-          expect(returnedGroup.users).toBeDefined();
-          expect(returnedGroup.users.length).toEqual(1);
-          let returnedUser = returnedGroup.users[0];
-          let originalUser = mockGroupUsersResponse.users[0];
-          expect(returnedUser.subjectId).toEqual(originalUser.subjectId);
-          expect(returnedUser.identityProvider).toEqual(originalUser.identityProvider);
-          expect(returnedUser.name).toEqual(originalUser.name);
+        service.addUserToCustomGroup(mockGroupUsersResponse.groupName, null).subscribe(returnedGroup => {
+          assertMockGroupUsersResponse(returnedGroup);
         });
 
         // simulate response from POST '/groups/Group 1/user' and flush response
         // to trigger subscription above
         const req = httpTestingController.expectOne(`${FabricAuthGroupService.baseGroupApiUrl}/${mockGroupUsersResponse.groupName}/users`);
         expect(req.request.method).toBe("POST");
-        req.flush(mockGroupUsersResponse, {status: 201, statusText: 'Created'});
-        
+        req.flush(mockGroupUsersResponse, {status: 201, statusText: 'Created'});        
         httpTestingController.verify();
       })
     )
@@ -116,8 +104,7 @@ fdescribe('FabricAuthGroupService', () => {
         // to trigger subscription above
         const req = httpTestingController.expectOne(`${FabricAuthGroupService.baseGroupApiUrl}/${mockGroupUsersResponse.groupName}/users`);
         expect(req.request.method).toBe("POST");
-        req.flush(null, {status: 404, statusText: 'Group not found'});
-        
+        req.flush(null, {status: 404, statusText: 'Group not found'});        
         httpTestingController.verify();
       })
     )
@@ -130,29 +117,16 @@ fdescribe('FabricAuthGroupService', () => {
         httpTestingController: HttpTestingController,
         service: FabricAuthGroupService) => {
 
-        let user = new User('Windows', 'Sub123');
-        user.name = 'First Last';
-
         // set up subscription to POST '/groups/Group 1/user' and service response expectations
-        service.removeUserFromCustomGroup(mockGroupUsersResponse.groupName, user).subscribe(returnedGroup => {
-          expect(returnedGroup.groupName).toBe(mockGroupUsersResponse.groupName);
-          expect(returnedGroup.groupSource).toBe(mockGroupUsersResponse.groupSource);
-          expect(returnedGroup.users).toBeDefined();
-          expect(returnedGroup.users.length).toEqual(1);
-          let returnedUser = returnedGroup.users[0];
-          let originalUser = mockGroupUsersResponse.users[0];
-          expect(returnedUser.subjectId).toEqual(originalUser.subjectId);
-          expect(returnedUser.identityProvider).toEqual(originalUser.identityProvider);
-          expect(returnedUser.name).toEqual(originalUser.name);
+        service.removeUserFromCustomGroup(mockGroupUsersResponse.groupName, null).subscribe(returnedGroup => {
+          assertMockGroupUsersResponse(returnedGroup);
         });
 
         // simulate response from DELETE '/groups/Group 1/user' and flush response
         // to trigger subscription above
         const req = httpTestingController.expectOne(`${FabricAuthGroupService.baseGroupApiUrl}/${mockGroupUsersResponse.groupName}/users`);
-        expect(req.request.method).toBe("DELETE");
-      
-        req.flush(mockGroupUsersResponse, {status: 204, statusText: 'No Content'});
-        
+        expect(req.request.method).toBe("DELETE");  
+        req.flush(mockGroupUsersResponse, {status: 204, statusText: 'No Content'});        
         httpTestingController.verify();
       })
     )
@@ -167,37 +141,95 @@ fdescribe('FabricAuthGroupService', () => {
 
         // set up subscription to GET '/groups/Group 1/roles' and service response expectations
         service.getGroupRoles(mockGroupRolesResponse.groupName).subscribe(returnedGroup => {
-          expect(returnedGroup).toBeDefined();
-          expect(returnedGroup.groupName).toBe('Dos Admin Group');
-          expect(returnedGroup.groupSource).toBe('Custom');
-          expect(returnedGroup.roles).toBeDefined();
-          expect(returnedGroup.roles.length).toBe(2);
-
-          let adminRole = returnedGroup.roles[0];
-          expect(adminRole.name).toBe('admin');
-          expect(adminRole.grain).toBe('dos');
-          expect(adminRole.securableItem).toBe('datamart');
-          expect(adminRole.parentRole).toBe('admin_parent');
-
-          let superUserRole = returnedGroup.roles[1];
-          expect(superUserRole.name).toBe('superuser');
-          expect(superUserRole.grain).toBe('dos');
-          expect(superUserRole.securableItem).toBe('datamart');
-          expect(superUserRole.childRoles).toBeDefined();
-          expect(superUserRole.childRoles.length).toBe(2);
-          expect(superUserRole.childRoles[0]).toBe('dos_child1');
-          expect(superUserRole.childRoles[1]).toBe('dos_child2');
+          assertMockGroupRolesResponse(returnedGroup);
         });
 
         // simulate response from GET '/groups/Dos Admin Group/roles' and flush response
         // to trigger subscription above
         const req = httpTestingController.expectOne(`${FabricAuthGroupService.baseGroupApiUrl}/${mockGroupRolesResponse.groupName}/roles`);
-        expect(req.request.method).toBe("GET");
-      
-        req.flush(mockGroupRolesResponse, {status: 200, statusText: 'OK'});
-        
+        expect(req.request.method).toBe("GET");      
+        req.flush(mockGroupRolesResponse, {status: 200, statusText: 'OK'});        
         httpTestingController.verify();
       })
     )
   );
+
+  fit('addRoleToGroup should deserialize all properties',
+    async(  
+      inject([HttpClient, HttpTestingController, FabricAuthGroupService], (
+        httpClient: HttpClient,
+        httpTestingController: HttpTestingController,
+        service: FabricAuthGroupService) => {
+        
+        // set up subscription to POST '/groups/Group 1/roles' and service response expectations
+        service.addRoleToGroup(mockGroupRolesResponse.groupName, null).subscribe(returnedGroup => {
+          assertMockGroupRolesResponse(returnedGroup);
+        });
+
+        // simulate response from GET '/groups/Dos Admin Group/roles' and flush response
+        // to trigger subscription above
+        const req = httpTestingController.expectOne(`${FabricAuthGroupService.baseGroupApiUrl}/${mockGroupRolesResponse.groupName}/roles`);
+        expect(req.request.method).toBe("POST");      
+        req.flush(mockGroupRolesResponse, {status: 201, statusText: 'Created'});
+        httpTestingController.verify();
+      })
+    )
+  );
+
+  fit('removeRoleFromGroup should deserialize all properties',
+    async(  
+      inject([HttpClient, HttpTestingController, FabricAuthGroupService], (
+        httpClient: HttpClient,
+        httpTestingController: HttpTestingController,
+        service: FabricAuthGroupService) => {
+
+        // set up subscription to DELETE '/groups/Group 1/roles' and service response expectations
+        service.removeRoleFromGroup(mockGroupRolesResponse.groupName, null).subscribe(returnedGroup => {
+          assertMockGroupRolesResponse(returnedGroup);
+        });
+
+        // simulate response from GET '/groups/Dos Admin Group/roles' and flush response
+        // to trigger subscription above
+        const req = httpTestingController.expectOne(`${FabricAuthGroupService.baseGroupApiUrl}/${mockGroupRolesResponse.groupName}/roles`);
+        expect(req.request.method).toBe("DELETE");      
+        req.flush(mockGroupRolesResponse, {status: 204, statusText: 'No Content'});        
+        httpTestingController.verify();
+      })
+    )
+  );
+
+  function assertMockGroupRolesResponse(returnedGroup) {
+    expect(returnedGroup).toBeDefined();
+    expect(returnedGroup.groupName).toBe('Dos Admin Group');
+    expect(returnedGroup.groupSource).toBe('Custom');
+    expect(returnedGroup.roles).toBeDefined();
+    expect(returnedGroup.roles.length).toBe(2);
+
+    let adminRole = returnedGroup.roles[0];
+    expect(adminRole.name).toBe('admin');
+    expect(adminRole.grain).toBe('dos');
+    expect(adminRole.securableItem).toBe('datamart');
+    expect(adminRole.parentRole).toBe('admin_parent');
+
+    let superUserRole = returnedGroup.roles[1];
+    expect(superUserRole.name).toBe('superuser');
+    expect(superUserRole.grain).toBe('dos');
+    expect(superUserRole.securableItem).toBe('datamart');
+    expect(superUserRole.childRoles).toBeDefined();
+    expect(superUserRole.childRoles.length).toBe(2);
+    expect(superUserRole.childRoles[0]).toBe('dos_child1');
+    expect(superUserRole.childRoles[1]).toBe('dos_child2');
+  }
+
+  function assertMockGroupUsersResponse(returnedGroup) {
+    expect(returnedGroup.groupName).toBe(mockGroupUsersResponse.groupName);
+    expect(returnedGroup.groupSource).toBe(mockGroupUsersResponse.groupSource);
+    expect(returnedGroup.users).toBeDefined();
+    expect(returnedGroup.users.length).toEqual(1);
+    let returnedUser = returnedGroup.users[0];
+    let originalUser = mockGroupUsersResponse.users[0];
+    expect(returnedUser.subjectId).toEqual(originalUser.subjectId);
+    expect(returnedUser.identityProvider).toEqual(originalUser.identityProvider);
+    expect(returnedUser.name).toEqual(originalUser.name);
+  }
 });
