6c30b24c35a54aebf3e22a36efc24151f5f5b701
Kyle Paul <djjoebanks@gmail.com>
2018-03-13 12:48:50 -0700

    added component

---
+++ Fabric.Authorization.AccessControl/src/app/modules/access-control/custom-group-add/custom-group-add.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CustomGroupAddComponent } from './custom-group-add.component';
+
+describe('CustomGroupAddComponent', () => {
+  let component: CustomGroupAddComponent;
+  let fixture: ComponentFixture<CustomGroupAddComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ CustomGroupAddComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CustomGroupAddComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ Fabric.Authorization.AccessControl/src/app/modules/access-control/custom-group-add/custom-group-add.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-group-add',
+  templateUrl: './custom-group-add.component.html',
+  styleUrls: ['./custom-group-add.component.css']
+})
+export class CustomGroupAddComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
