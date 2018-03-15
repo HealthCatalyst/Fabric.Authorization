6b97791f6a829e67f439db6e65e58829d35cd37e
John Parsons <john.parsons@healthcatalyst.com>
2018-03-15 11:27:02 -0600

    fixed log file naming issue in script

--- Fabric.Authorization.SqlServer/Storage/HCFabricAuthorizationLogFile1.sql
+++ Fabric.Authorization.SqlServer/Storage/HCFabricAuthorizationLogFile1.sql
@@ -7,7 +7,7 @@ ALTER DATABASE [$(DatabaseName)]
 ADD LOG FILE
 (
 	NAME = [HCFabricAuthorizationLogFile1],
-	FILENAME = '$(FabricAuthorizationLogFileMountPoint)\HC$(DatabaseName)LogFile1.ldf',
+	FILENAME = '$(FabricAuthorizationLogMountPoint)\HC$(DatabaseName)LogFile1.ldf',
 	SIZE = 100 MB,
 	FILEGROWTH = 10%
 )
