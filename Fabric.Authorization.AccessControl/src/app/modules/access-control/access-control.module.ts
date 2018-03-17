e48a93775bbd21c21b84fcf1fe38df3d668fc15f
Joe Skeen <joe.skeen@healthcatalyst.com>
2018-03-16 20:00:01 -0600

    Add Fabric.Cashmere and start to use components/styles from it on the AccessControl demo page

--- Fabric.Authorization.AccessControl/src/app/modules/access-control/access-control.module.ts
+++ Fabric.Authorization.AccessControl/src/app/modules/access-control/access-control.module.ts
@@ -2,27 +2,37 @@ import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { ButtonModule, IconModule, PopoverModule } from '@healthcatalyst/cashmere';
 
 import { AccessControlRoutingModule } from './access-control-routing.module';
-import { FabricBaseService, 
-         FabricAuthGroupService, 
-         FabricAuthMemberSearchService, 
-         FabricAuthUserService, 
-         FabricExternalIdpSearchService,
-         FabricAuthRoleService } from '../../services';
+import {
+  FabricBaseService,
+  FabricAuthGroupService,
+  FabricAuthMemberSearchService,
+  FabricAuthUserService,
+  FabricExternalIdpSearchService,
+  FabricAuthRoleService
+} from '../../services';
 
 import { MemberListComponent } from './member-list/member-list.component';
 import { MemberAddComponent } from './member-add/member-add.component';
 import { CustomGroupAddComponent } from './custom-group-add/custom-group-add.component';
 
-
 @NgModule({
   imports: [
     CommonModule,
     AccessControlRoutingModule,
-    FormsModule
+    FormsModule,
+    // Cashmere modules
+    ButtonModule,
+    IconModule,
+    PopoverModule
   ],
-  declarations: [MemberListComponent, MemberAddComponent, CustomGroupAddComponent],  
+  declarations: [
+    MemberListComponent,
+    MemberAddComponent,
+    CustomGroupAddComponent
+  ],
   providers: [
     FabricBaseService,
     FabricAuthGroupService,
@@ -31,7 +41,6 @@ import { CustomGroupAddComponent } from './custom-group-add/custom-group-add.com
     FabricExternalIdpSearchService,
     FabricAuthRoleService
   ],
-  exports:[
-  ]
+  exports: []
 })
-export class AccessControlModule { }
+export class AccessControlModule {}
