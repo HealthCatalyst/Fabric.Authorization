a5d5d0107499d20a1d1adf835682878cddb69df8
Brian Smith <brian.smith@healthcatalyst.com>
2018-03-29 17:02:34 -0400

    added displayName and description to group and role models

--- Fabric.Authorization.AccessControl/src/app/models/group.model.ts
+++ Fabric.Authorization.AccessControl/src/app/models/group.model.ts
@@ -2,6 +2,8 @@ import { Role, User } from '../models';
 
 export class Group {
   public id: string;
+  public displayName: string;
+  public description: string;
   public roles: Array<Role>;
   public users: Array<User> = new Array<User>();
 
