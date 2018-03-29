4c698f985f60b2e79b02fdeb5871a25453d5dc70
Kyle Paul <djjoebanks@gmail.com>
2018-03-29 15:21:38 -0700

    updates to interceptors to get packaging to work

--- Fabric.Authorization.AccessControl/src/app/modules/access-control/access-control.module.ts
+++ Fabric.Authorization.AccessControl/src/app/modules/access-control/access-control.module.ts
@@ -2,6 +2,7 @@ import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+<<<<<<< HEAD
 import {
   ButtonModule,
   IconModule,
@@ -10,6 +11,9 @@ import {
   SelectModule,
   ProgressIndicatorsModule
 } from '@healthcatalyst/cashmere';
+=======
+import { ButtonModule, IconModule, PopoverModule } from '@healthcatalyst/cashmere';
+>>>>>>> updates to interceptors to get packaging to work
 
 import { AccessControlRoutingModule } from './access-control-routing.module';
 import {
@@ -21,6 +25,8 @@ import {
   FabricAuthRoleService,
   AccessControlConfigService
 } from '../../services';
+
+// import { httpInterceptorProviders } from '../../services/interceptors';
 
 import { MemberListComponent } from './member-list/member-list.component';
 import { MemberAddComponent } from './member-add/member-add.component';
@@ -49,6 +55,7 @@ import { CustomGroupEditComponent } from './custom-group-edit/custom-group-edit.
     CustomGroupEditComponent
   ],
   providers: [
+  //  httpInterceptorProviders,
     FabricBaseService,
     FabricAuthGroupService,
     FabricAuthMemberSearchService,
@@ -58,4 +65,13 @@ import { CustomGroupEditComponent } from './custom-group-edit/custom-group-edit.
   ],
   exports: []
 })
-export class AccessControlModule { }
+export class AccessControlModule {
+  static forRoot(config: AccessControlConfigService): ModuleWithProviders {
+    return {
+      ngModule: AccessControlModule,
+      providers: [
+        {provide: AccessControlConfigService, useValue: config}
+      ]
+    };
+  }
+}
