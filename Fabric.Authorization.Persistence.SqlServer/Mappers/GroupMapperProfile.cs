dffa35699a3160ea69ba5b03777d2e82d7cf927d
Brian Smith <brian.smith@healthcatalyst.com>
2018-03-29 11:15:16 -0400

    added integration tests for new patch endpoints

--- Fabric.Authorization.Persistence.SqlServer/Mappers/GroupMapperProfile.cs
+++ Fabric.Authorization.Persistence.SqlServer/Mappers/GroupMapperProfile.cs
@@ -1,5 +1,6 @@
 ﻿using System.Linq;
 using AutoMapper;
+using Fabric.Authorization.Persistence.SqlServer.EntityModels;
 
 namespace Fabric.Authorization.Persistence.SqlServer.Mappers
 {
@@ -7,11 +8,12 @@ namespace Fabric.Authorization.Persistence.SqlServer.Mappers
     {
         public GroupMapperProfile()
         {
-            CreateMap<EntityModels.Group, Domain.Models.Group>()
+            CreateMap<Group, Domain.Models.Group>()
                 .ForMember(x => x.Id, opt => opt.MapFrom(src => src.GroupId))
                 .ForMember(x => x.Roles, opt => opt.MapFrom(src => src.GroupRoles.Select(gr => gr.Role)))
                 .ForMember(x => x.Users, opt => opt.MapFrom(src => src.GroupUsers.Select(gu => gu.User)))
                 .ReverseMap()
+                .ForMember(x => x.GroupId, opt => opt.MapFrom(src => src.Id))
                 .ForMember(x => x.Id, opt => opt.Ignore());
         }
     }
