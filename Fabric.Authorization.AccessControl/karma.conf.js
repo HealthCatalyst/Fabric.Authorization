dca8a32fa809ff081d4658dcb00cee1970b2109d
Kyle Paul <djjoebanks@gmail.com>
2018-04-03 09:38:58 -0700

    switched to using chrome headless instead of phantom js for karma tests

--- Fabric.Authorization.AccessControl/karma.conf.js
+++ Fabric.Authorization.AccessControl/karma.conf.js
@@ -1,14 +1,15 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
+process.env.CHROME_BIN = require("puppeteer").executablePath();
+
 module.exports = function (config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular/cli'],
     plugins: [
       require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-phantomjs-launcher'), 
+      require('karma-chrome-launcher'),      
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular/cli/plugins/karma')
@@ -28,7 +29,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: [ 'Chrome', 'PhantomJS' ],
+    browsers: [ 'Chrome'],
     singleRun: false
   });
 };
