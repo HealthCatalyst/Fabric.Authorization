7f6a2264728f5fb99ed29ee5f82f76b79509c3ea
Kyle Paul <djjoebanks@gmail.com>
2018-04-02 21:01:31 -0700

    fix member search tests

--- Fabric.Authorization.AccessControl/src/app/services/fabric-auth-member-search.service.mock.ts
+++ Fabric.Authorization.AccessControl/src/app/services/fabric-auth-member-search.service.mock.ts
@@ -1,30 +1,33 @@
-import { IAuthMemberSearchResult } from '../models';
+import { IAuthMemberSearchResponse, IAuthMemberSearchResult } from '../models';
 
-export const mockAuthSearchResult: IAuthMemberSearchResult[] = [
-    {
-        subjectId: 'sub123',
-        identityProvider: 'AD',
-        firstName: 'First',
-        middleName: 'Middle',
-        lastName: 'Last',
-        groupName: 'Group 1',
-        roles: [
-            { name: 'admin', grain: 'app', securableItem: 'foo' },
-            { name: 'superuser', grain: 'app', securableItem: 'foo' }
-        ],
-        entityType: 'User'
-    },
-    {
-        subjectId: 'sub345',
-        identityProvider: 'AD',
-        firstName: 'First2',
-        middleName: 'Middle2',
-        lastName: 'Last2',
-        groupName: 'Group 2',
-        roles: [{ name: 'viewer', grain: 'app', securableItem: 'foo' }],
-        entityType: 'CustomGroup'
-    }
-];
+export const mockAuthSearchResult: IAuthMemberSearchResponse = {
+    totalCount: 2,
+    results: [
+        {
+            subjectId: 'sub123',
+            identityProvider: 'AD',
+            firstName: 'First',
+            middleName: 'Middle',
+            lastName: 'Last',
+            groupName: 'Group 1',
+            roles: [
+                { name: 'admin', grain: 'app', securableItem: 'foo' },
+                { name: 'superuser', grain: 'app', securableItem: 'foo' }
+            ],
+            entityType: 'User'
+        },
+        {
+            subjectId: 'sub345',
+            identityProvider: 'AD',
+            firstName: 'First2',
+            middleName: 'Middle2',
+            lastName: 'Last2',
+            groupName: 'Group 2',
+            roles: [{ name: 'viewer', grain: 'app', securableItem: 'foo' }],
+            entityType: 'CustomGroup'
+        }
+    ]
+};
 
 export class FabricAuthMemberSearchServiceMock {
     searchMembers: jasmine.Spy = jasmine.createSpy('searchMembers');
