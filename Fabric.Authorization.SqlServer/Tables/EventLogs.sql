c2e2eda4ab71fb90dd82a2aa45bef8c680d24832
Brian Smith <brian.smith@healthcatalyst.com>
2018-04-18 15:37:53 -0400

    added SQL script to create EventLogs table

---
+++ Fabric.Authorization.SqlServer/Tables/EventLogs.sql
@@ -0,0 +1,19 @@
+﻿
+CREATE TABLE [dbo].[EventLogs]
+(
+	[Id] int IDENTITY(1,1) NOT NULL,
+	[Message] nvarchar(max) NULL,
+	[MessageTemplate] nvarchar(max) NULL,
+	[Level] nvarchar(128) NULL,
+	[TimeStamp] datetimeoffset(7) NOT NULL,  -- use datetime for SQL Server pre-2008
+	[Exception] nvarchar(max) NULL,
+	[Properties] xml NULL,
+	[LogEvent] nvarchar(max) NULL
+
+	CONSTRAINT [PK_Logs] 
+		PRIMARY KEY CLUSTERED ([Id] ASC) 
+		WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF,
+			ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) 
+		ON [HCFabricAuthorizationData1]
+
+) ON [HCFabricAuthorizationData1];
