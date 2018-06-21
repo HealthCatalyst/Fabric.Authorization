2ccac0f10442567016e4113d6bbcac7b4f38ff53
Brian Smith <brian.smith@healthcatalyst.com>
2018-06-21 14:38:38 -0400

    added operations for client and roles

--- Fabric.Authorization.Client/AuthorizationClient.cs
+++ Fabric.Authorization.Client/AuthorizationClient.cs
@@ -1,4 +1,6 @@
-﻿using System.Net.Http;
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using Fabric.Authorization.Client.Routes;
 using Fabric.Authorization.Models;
@@ -15,12 +17,79 @@ namespace Fabric.Authorization.Client
             this.client = client;
         }
 
+
+        #region Clients
         public async Task<UserApiModel> GetPermissionsForCurrentUser()
         {
             var route = new UserRouteBuilder().UserPermissionsRoute;
             var message = new HttpRequestMessage(HttpMethod.Get, route);
             return await SendAndParseJson<UserApiModel>(message).ConfigureAwait(false);
         }
+
+        public async Task<ClientApiModel> AddClient(ClientApiModel clientModel)
+        {
+            var message = new HttpRequestMessage(HttpMethod.Post, new ClientRouteBuilder().Route)
+            {
+                Content = new StringContent(JsonConvert.SerializeObject(clientModel), Encoding.UTF8, "application/json")
+            };
+
+            return await SendAndParseJson<ClientApiModel>(message).ConfigureAwait(false);
+        }
+
+        public async Task<ClientApiModel> GetClient(string clientId)
+        {
+            var message = new HttpRequestMessage(HttpMethod.Get, new ClientRouteBuilder().ClientId(clientId).ToString());
+            return await SendAndParseJson<ClientApiModel>(message).ConfigureAwait(false);
+        }
+
+        #endregion
+
+        #region Roles
+
+        public async Task<RoleApiModel> AddRole(RoleApiModel roleModel)
+        {
+            var message = new HttpRequestMessage(HttpMethod.Post, new RoleRouteBuilder().Route)
+            {
+                Content = new StringContent(JsonConvert.SerializeObject(roleModel), Encoding.UTF8, "application/json")
+            };
+
+            return await SendAndParseJson<RoleApiModel>(message).ConfigureAwait(false);
+        }
+
+        public async Task<RoleApiModel> AddPermissionToRole(string roleId, List<PermissionApiModel> permissionModels)
+        {
+            var message = new HttpRequestMessage(HttpMethod.Post, new RoleRouteBuilder().RoleId(roleId).ToString())
+            {
+                Content = new StringContent(JsonConvert.SerializeObject(permissionModels), Encoding.UTF8, "application/json")
+            };
+
+            return await SendAndParseJson<RoleApiModel>(message).ConfigureAwait(false);
+        }
+
+        public async Task<RoleApiModel> DeletePermissionFromRole(string roleId, List<PermissionApiModel> permissionModels)
+        {
+            var message = new HttpRequestMessage(HttpMethod.Delete, new RoleRouteBuilder().RoleId(roleId).ToString())
+            {
+                Content = new StringContent(JsonConvert.SerializeObject(permissionModels), Encoding.UTF8, "application/json")
+            };
+
+            return await SendAndParseJson<RoleApiModel>(message).ConfigureAwait(false);
+        }
+
+        public async Task<UserApiModel> GetRole(string roleId)
+        {
+            var message = new HttpRequestMessage(HttpMethod.Get, new RoleRouteBuilder().RoleId(roleId).ToString());
+            return await SendAndParseJson<UserApiModel>(message).ConfigureAwait(false);
+        }
+
+        public async Task<UserApiModel> GetRole(string grain, string securableItem, string roleName)
+        {
+            var message = new HttpRequestMessage(HttpMethod.Get,
+                new RoleRouteBuilder().Grain(grain).SecurableItem(securableItem).Name(roleName).ToString());
+            return await SendAndParseJson<UserApiModel>(message).ConfigureAwait(false);
+        }
+
+        #endregion
 
         private async Task<T> SendAndParseJson<T>(HttpRequestMessage message)
         {
