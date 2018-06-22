beb0ab5c0455d7222aa3cbffa48ab79666e30691
Brian Smith <brian.smith@healthcatalyst.com>
2018-06-22 16:26:55 -0400

    added unit tests for ClientRouteBuilder

--- Fabric.Authorization.Client.UnitTests/Routes/ClientRouteTests.cs
+++ Fabric.Authorization.Client.UnitTests/Routes/ClientRouteTests.cs
@@ -1,6 +1,22 @@
-﻿namespace Fabric.Authorization.Client.UnitTests.Routes
+﻿using Fabric.Authorization.Client.Routes;
+using Xunit;
+
+namespace Fabric.Authorization.Client.UnitTests.Routes
 {
     public class ClientRouteTests
     {
+        [Fact]
+        public void ClientRouteBuilder_NoClientId_Success()
+        {
+            var route = new ClientRouteBuilder().Route;
+            Assert.Equal($"/{RouteConstants.ClientCollectionRoute}", route);
+        }
+
+        [Fact]
+        public void ClientRouteBuilder_WithClientId_Success()
+        {
+            var route = new ClientRouteBuilder().ClientId("client_id").Route;
+            Assert.Equal($"/{RouteConstants.ClientCollectionRoute}/client_id", route);
+        }
     }
 }
