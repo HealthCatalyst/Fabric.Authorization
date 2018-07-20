908ec6eba63b9532742c89bdc3dd425c38560813
Brian Smith <brian.smith@healthcatalyst.com>
2018-07-20 08:11:17 -0400

    set up access control UI in authorization

---
+++ Fabric.Authorization.API/Modules/RootModule.cs
@@ -0,0 +1,17 @@
+﻿using Nancy;
+
+namespace Fabric.Authorization.API.Modules
+{
+    public class RootModule : NancyModule
+    {
+        public RootModule(): base("/v1")
+        {
+            Get("/", _ => Redirect(), null, "Redirect");
+        }
+
+        private dynamic Redirect()
+        {
+            return Response.AsRedirect("/client/index.html");
+        }
+    }
+}
