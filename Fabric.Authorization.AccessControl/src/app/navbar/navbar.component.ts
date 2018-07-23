8d14147bde5b4bef6e3b6c82f30b6095d7ad2bba
michael.vidal <michael.vidal@healthcatalyst.com>
2018-07-23 16:59:04 -0400

    Added the ability to get the user's name after login and display on the nav bar.

--- Fabric.Authorization.AccessControl/src/app/navbar/navbar.component.ts
+++ Fabric.Authorization.AccessControl/src/app/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+
+import { AuthService } from '../services/global/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  userDisplayName: string;
 
-  constructor() { }
+  constructor(
+    private authService: AuthService
+  ) {}
 
   ngOnInit() {
+    this.authService.getUser().then(result => {
+      if (result && result.profile) {
+        debugger;
+        if(result.profile.family_name && result.profile.given_name){
+          this.userDisplayName = result.profile.given_name + ' ' + result.profile.family_name;
+        }else{
+          this.userDisplayName = result.profile.name;
+        }
+      }
+    });
   }
-
 }
