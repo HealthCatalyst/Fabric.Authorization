e2205fcd304cbe125ceffa7f10b936296a8a0299
michael.vidal <michael.vidal@healthcatalyst.com>
2018-07-23 13:58:30 -0400

    Removed components that are no longer necessary.

--- Fabric.Authorization.AccessControl/src/app/app-routing.module.ts
+++ Fabric.Authorization.AccessControl/src/app/app-routing.module.ts
@@ -1,9 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { LogoutComponent } from './logout/logout.component';
 import { AuthenticationGuard } from './services/guards/authentication.guard';
 import { AuthService } from './services/global/auth.service';
 
@@ -18,18 +15,6 @@ const routes: Routes = [
     path: '',
     redirectTo: 'access-control',
     pathMatch: 'full'
-  },
-  {
-    path: 'home',
-    component: HomeComponent
-  },
-  {
-    path: 'login',
-    component: LoginComponent
-  },
-  {
-    path: 'logout',
-    component: LogoutComponent
   }
 ];
 
