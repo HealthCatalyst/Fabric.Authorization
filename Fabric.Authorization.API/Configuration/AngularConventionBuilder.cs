176433577fb6e53361d24f06ab868d48af528f7f
michael.vidal <michael.vidal@healthcatalyst.com>
2018-07-27 16:27:47 -0400

    Added an AngularConventionBuilder so that Nancy serves up the index.html file for requests to the angular routes.

---
+++ Fabric.Authorization.API/Configuration/AngularConventionBuilder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using Nancy;
+using Nancy.Responses;
+
+namespace Fabric.Authorization.API.Configuration
+{
+    public class AngularConventionBuilder
+    {
+        public static Func<NancyContext, string, Response> AddAngularRoot(string angularRootDirectory, string indexFile)
+        {
+            return (ctx, appRoot) =>
+            {
+                if (!ctx.Request.Path.StartsWith($"/{angularRootDirectory}/")) return null;
+                var file = Path.Combine(appRoot, angularRootDirectory, indexFile);
+                return new GenericFileResponse(file, ctx);
+            };
+        }
+    }
+}
