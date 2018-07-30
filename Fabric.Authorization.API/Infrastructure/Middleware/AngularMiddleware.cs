4b229bbba0b390ff62bda96cf9bb8b9bd3c888ca
michael.vidal <michael.vidal@healthcatalyst.com>
2018-07-30 10:14:57 -0400

    Refactored startup.cs to extract inline middleware into its own class.

---
+++ Fabric.Authorization.API/Infrastructure/Middleware/AngularMiddleware.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Fabric.Authorization.API.Constants;
+using Microsoft.AspNetCore.Http;
+
+namespace Fabric.Authorization.API.Infrastructure.Middleware
+{
+    public class AngularMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public AngularMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            await _next(context);
+            var accessControlPath = $"/{AccessControl.Path}/";
+            if (context.Response.StatusCode == 404 &&
+                !Path.HasExtension(context.Request.Path.Value) &&
+                context.Request.Path.Value.StartsWith(accessControlPath))
+            {
+                context.Request.Path = $"{accessControlPath}{AccessControl.Index}";
+                await _next(context);
+            }
+        }
+    }
+}
---
+++ Fabric.Authorization.API/Infrastructure/Middleware/AngularMiddlewareExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+
+namespace Fabric.Authorization.API.Infrastructure.Middleware
+{
+    public static class AngularMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseAngular(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<AngularMiddleware>();
+        }
+    }
+}
