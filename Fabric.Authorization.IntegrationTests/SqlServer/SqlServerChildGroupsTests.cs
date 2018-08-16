ffc4e17136dc29cfc152f9db2eb99b58303303e9
Brian Smith <brian.smith@healthcatalyst.com>
2018-08-16 00:59:06 -0400

    fixed Collection attribute name

--- Fabric.Authorization.IntegrationTests/SqlServer/SqlServerChildGroupsTests.cs
+++ Fabric.Authorization.IntegrationTests/SqlServer/SqlServerChildGroupsTests.cs
@@ -4,7 +4,7 @@ using Xunit;
 
 namespace Fabric.Authorization.IntegrationTests.SqlServer
 {
-    [Collection("SqlServerChildGroupsTests")]
+    [Collection("SqlServerTests")]
     public class SqlServerChildGroupsTests : ChildGroupsTests
     {
         public SqlServerChildGroupsTests(IntegrationTestsFixture fixture, SqlServerIntegrationTestsFixture sqlFixture) : base(fixture, StorageProviders.SqlServer, sqlFixture.ConnectionStrings)
