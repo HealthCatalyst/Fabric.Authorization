730e0804229dc265e813650d5b7801b74aa4a409
Brian Smith <brian.smith@healthcatalyst.com>
2018-08-30 17:54:15 -0400

    expire user permission cache after 2 minutes

--- Fabric.Authorization.AccessControl/src/app/services/current-user.service.ts
+++ Fabric.Authorization.AccessControl/src/app/services/current-user.service.ts
@@ -1,20 +1,21 @@
 import { Injectable } from '@angular/core';
 import { FabricAuthUserService } from './fabric-auth-user.service';
-import { Observable } from '../../../node_modules/rxjs';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CurrentUserService {
   private permissions: Array<string> = [];
-  private initialized: boolean;
+  private lastUpdateTimestamp = 0;
 
   constructor(private authUserService: FabricAuthUserService) {
   }
 
   getPermissions(): Observable<string[]> {
-    if (!this.initialized) {
-      this.initialized = true;
+    const currentTimeStamp = new Date().getTime();
+    if (this.lastUpdateTimestamp === 0 || (Math.abs(currentTimeStamp - this.lastUpdateTimestamp) / 60000) > 2) {
+      this.lastUpdateTimestamp = currentTimeStamp;
       return this.authUserService.getCurrentUserPermissions().do(userPermissionResponse => {
         this.permissions = userPermissionResponse.permissions;
       }).map(p => p.permissions);
