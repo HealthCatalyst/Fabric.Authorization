6d10d90d8909db90fe7c777f644b1697562d2f90
Jason Soto <jason.soto@healthcatalyst.com>
2018-09-17 17:29:35 -0600

    IE no-cache required headers
    Do not cache any requests made from access-control

---
+++ Fabric.Authorization.AccessControl/src/app/services/interceptors/standard-header-interceptor.ts
@@ -0,0 +1,22 @@
+import { Injectable, Injector } from '@angular/core';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class StandardHeaderInterceptor implements HttpInterceptor {
+    constructor(private injector: Injector) {}
+
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        // IE requires Pragma, Expires, and If-Modified-Since headers to NOT cache results, so we need to add these headers on requests
+        const genericReq: HttpRequest<any> = req.clone({
+            headers: req.headers
+                .set('Accept', 'application/json')
+                .set('Content-Type', 'application/json')
+                .set('Pragma', 'no-cache')
+                .set('Expires', 'Sat, 01 Jan 2000 00:00:00 GMT')
+                .set('If-Modified-Since', '0')
+        });
+
+        return next.handle(genericReq);
+    }
+}
