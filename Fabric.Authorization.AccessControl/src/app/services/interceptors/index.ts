535447527c12380e26478e8bf68bd814a8d9f2a8
Jason Soto <jason.soto@healthcatalyst.com>
2018-09-18 09:47:01 -0600

    Remove headers from standard-header-interceptor service
    Headers exists in fabric-http-request-interceptor service
    Add standard-header-interceptor to index.ts

--- Fabric.Authorization.AccessControl/src/app/services/interceptors/index.ts
+++ Fabric.Authorization.AccessControl/src/app/services/interceptors/index.ts
@@ -2,6 +2,7 @@
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { FabricHttpRequestInterceptorService } from './fabric-http-request-interceptor.service';
 import { FabricHttpErrorHandlerInterceptorService } from './fabric-http-error-handler-interceptor.service';
+import { StandardHeaderInterceptor } from './standard-header-interceptor';
 
 /** Http interceptor providers in outside-in order */
 export const httpInterceptorProviders = [
@@ -13,6 +14,11 @@ export const httpInterceptorProviders = [
   {
     provide: HTTP_INTERCEPTORS,
     useClass: FabricHttpErrorHandlerInterceptorService,
+    multi: true
+  },
+  {
+    provide: HTTP_INTERCEPTORS,
+    useClass: StandardHeaderInterceptor,
     multi: true
   }
 ];
