cfcdaff130c40e2b3df5f75ba9f1f38da2d22a3d
Brian Smith <brian.smith@healthcatalyst.com>
2018-10-18 10:53:54 -0400

    added IdP search provider and supporting model classes

---
+++ Fabric.Authorization.API/RemoteServices/IdentityProviderSearch/Providers/IdPSearchProvider.cs
@@ -0,0 +1,34 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Fabric.Authorization.API.Configuration;
+using Fabric.Authorization.API.RemoteServices.IdentityProviderSearch.Models;
+using Fabric.Platform.Http;
+using Serilog;
+
+namespace Fabric.Authorization.API.RemoteServices.IdentityProviderSearch.Providers
+{
+    public class IdPSearchProvider : IIdPSearchProvider
+    {
+        private readonly HttpClient _httpClient;
+        private readonly IHttpRequestMessageFactory _httpRequestMessageFactory;
+        private readonly IAppConfiguration _appConfiguration;
+        private readonly ILogger _logger;
+
+        public IdPSearchProvider(
+            HttpClient httpClient,
+            IHttpRequestMessageFactory httpRequestMessageFactory,
+            IAppConfiguration appConfiguration,
+            ILogger logger)
+        {
+            _httpClient = httpClient;
+            _httpRequestMessageFactory = httpRequestMessageFactory;
+            _appConfiguration = appConfiguration;
+            _logger = logger;
+        }
+
+        public Task<IdPPrincipalSearchResponse> Search(IdPPrincipalSearchRequest request)
+        {
+
+        }
+    }
+}
---
+++ Fabric.Authorization.API/Services/IdPSearchService.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+using Fabric.Authorization.API.RemoteServices.IdentityProviderSearch.Models;
+using Fabric.Authorization.API.RemoteServices.IdentityProviderSearch.Providers;
+
+namespace Fabric.Authorization.API.Services
+{
+    public class IdPSearchService
+    {
+        private readonly IIdPSearchProvider _idPSearchProvider;
+
+        public IdPSearchService(IIdPSearchProvider idPSearchProvider)
+        {
+            _idPSearchProvider = idPSearchProvider;
+        }
+
+        /// <summary>
+        /// TODO: need to specify exact search
+        /// </summary>
+        /// <param name="groupName"></param>
+        /// <returns></returns>
+        public async Task<IdPPrincipalSearchResponse> GetGroup(string groupName)
+        {
+            var result = await _idPSearchProvider.Search(new IdPPrincipalSearchRequest
+            {
+                SearchText = groupName,
+                Type = SearchType.Group.ToString()
+            });
+
+            return result;
+        }
+    }
+
+    public enum SearchType
+    {
+        User,
+        Group,
+        UserAndGroup
+    }
+}
