6f5966144025aa3b1c749b2f180488a7983ea0ca
Brian Smith <brian.smith@healthcatalyst.com>
2018-10-19 08:55:28 -0400

    added constants file for identity providers

---
+++ Fabric.Authorization.API/Constants/IdentityProviders.cs
@@ -0,0 +1,8 @@
+﻿namespace Fabric.Authorization.API.Constants
+{
+    public static class IdentityProviders
+    {
+        public static readonly string ActiveDirectory = "Windows";
+        public static readonly string AzureActiveDirectory = "AzureActiveDirectory";
+    }
+}
