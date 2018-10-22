b7653f1f9750ef76e8a69e3f2bda0e7227646996
Brian Smith <brian.smith@healthcatalyst.com>
2018-10-21 21:48:00 -0400

    disable DiscoveryService by default so that app can launch on build server

---
+++ Fabric.Authorization.API/Configuration/AuthDiscoveryServiceSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Fabric.Authorization.API.Configuration
+{
+    public class AuthDiscoveryServiceSettings
+    {
+        public bool UseDiscovery { get; set; }
+        public string DiscoveryServiceEndpoint { get; set; }
+    }
+}
