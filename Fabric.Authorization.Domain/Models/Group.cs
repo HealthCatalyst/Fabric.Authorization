4e73f65726c074278cce2566f93a3c3641b992f1
Brian Smith <brian.smith@healthcatalyst.com>
2018-10-27 11:48:12 -0400

    implemented GroupIdentifier IEqualityComparer to fix some tests

--- Fabric.Authorization.Domain/Models/Group.cs
+++ Fabric.Authorization.Domain/Models/Group.cs
@@ -118,5 +118,40 @@ namespace Fabric.Authorization.Domain.Models
         {
             return $"IdP = {IdentityProvider}, TenantId = {TenantId}, GroupName = {GroupName}";
         }
+
+
+        public override int GetHashCode()
+        {
+            return ToString().GetHashCode();
+        }
+    }
+
+    public class GroupIdentifierComparer : IEqualityComparer<GroupIdentifier>
+    {
+        public bool Equals(GroupIdentifier g1, GroupIdentifier g2)
+        {
+            if (g1 == g2)
+            {
+                return true;
+            }
+
+            if (g1 == null || g2 == null)
+            {
+                return false;
+            }
+
+            return string.Equals(g1.IdentityProvider, g2.IdentityProvider)
+                   && string.Equals(g1.TenantId, g2.TenantId)
+                   && string.Equals(g1.GroupName, g2.GroupName);
+        }
+
+        public int GetHashCode(GroupIdentifier groupIdentifier)
+        {
+            var hash = 13;
+            hash = (hash * 7) + groupIdentifier.IdentityProvider.GetHashCode();
+            hash = (hash * 7) + groupIdentifier.TenantId.GetHashCode();
+            hash = (hash * 7) + groupIdentifier.GroupName.GetHashCode();
+            return hash;
+        }
     }
 }
