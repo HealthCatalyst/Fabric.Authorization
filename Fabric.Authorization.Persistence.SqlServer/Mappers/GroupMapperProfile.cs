47e099f40fde2d4fae159c23f02b151374267e54
Brian Smith <brian.smith@healthcatalyst.com>
2018-11-16 09:32:01 -0500

    updated AutoMapper profile for groups so that we can map domain model child collections to corresponding many to many EF models to allow a single DB call during migration

--- Fabric.Authorization.Persistence.SqlServer/Mappers/GroupMapperProfile.cs
+++ Fabric.Authorization.Persistence.SqlServer/Mappers/GroupMapperProfile.cs
@@ -1,4 +1,5 @@
-﻿using AutoMapper;
+﻿using System.Linq;
+using AutoMapper;
 using Fabric.Authorization.Persistence.SqlServer.EntityModels;
 
 namespace Fabric.Authorization.Persistence.SqlServer.Mappers
@@ -12,7 +13,32 @@ namespace Fabric.Authorization.Persistence.SqlServer.Mappers
                 .ForMember(x => x.Roles, opt => opt.MapFrom(src => src.Roles))
                 .ForMember(x => x.Users, opt => opt.MapFrom(src => src.Users))
                 .ReverseMap()
-                .ForMember(x => x.Id, opt => opt.Ignore());
+                .ForMember(x => x.Id, opt => opt.Ignore())
+                .ForMember(x => x.GroupRoles, opt => opt.MapFrom(src =>
+                    src.Roles.Select(r => new GroupRole
+                    {
+                        RoleId = r.Id,
+                        GroupId = src.Id
+                    })))
+                .ForMember(x => x.GroupUsers, opt => opt.MapFrom(src =>
+                    src.Users.Select(u => new GroupUser
+                    {
+                        SubjectId = u.SubjectId,
+                        IdentityProvider = u.IdentityProvider,
+                        GroupId = src.Id
+                    })))
+                .ForMember(x => x.ChildGroups, opt => opt.MapFrom(src =>
+                    src.Children.Select(c => new ChildGroup
+                    {
+                        ParentGroupId = src.Id,
+                        ChildGroupId = c.Id
+                    })))
+                .ForMember(x => x.ChildGroups, opt => opt.MapFrom(src =>
+                    src.Parents.Select(p => new ChildGroup
+                    {
+                        ParentGroupId = p.Id,
+                        ChildGroupId = src.Id
+                    })));
         }
     }
 }
