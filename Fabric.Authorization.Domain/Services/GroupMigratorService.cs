c1eff89d7e21a9223713a38f6e66813ac4acc5dd
Brian Smith <brian.smith@healthcatalyst.com>
2019-01-09 13:57:33 -0500

    fixed bug with IdP migration logic (needed case insensitive comparison for group source)

--- Fabric.Authorization.Domain/Services/GroupMigratorService.cs
+++ Fabric.Authorization.Domain/Services/GroupMigratorService.cs
@@ -163,7 +163,7 @@ namespace Fabric.Authorization.Domain.Services
 
         public async Task MigrateIdentityProvider()
         {
-            var groups = (await _groupStore.GetAll()).Where(g => g.Source == GroupConstants.DirectorySource && g.IdentityProvider == null).ToList();
+            var groups = (await _groupStore.GetAll()).Where(g => string.Equals(g.Source, GroupConstants.DirectorySource, StringComparison.OrdinalIgnoreCase) && g.IdentityProvider == null).ToList();
 
             foreach (var group in groups)
             {
