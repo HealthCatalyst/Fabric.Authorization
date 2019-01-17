d1e0ae982171e69bae7580ce85ca4ad3fbdc9ab8
Jason Soto <jason.soto@healthcatalyst.com>
2019-01-17 16:01:09 -0700

    Update idpss search mock

--- Fabric.Authorization.AccessControl/src/app/services/fabric-external-idp-search.service.mock.ts
+++ Fabric.Authorization.AccessControl/src/app/services/fabric-external-idp-search.service.mock.ts
@@ -8,14 +8,31 @@ export const mockExternalIdpSearchResult: IdPSearchResult = {
         firstName: 'First_1',
         middleName: '',
         lastName: 'Last_1',
-        principalType: 'user'
+        principalType: 'user',
+        tenantId: null,
+        identityProvider: 'Windows',
+        identityProviderUserPrincipalName: 'sub123'
       },
       {
         subjectId: 'sub456',
         firstName: 'First_2',
         middleName: '',
         lastName: 'Last_2',
-        principalType: 'user'
+        principalType: 'user',
+        tenantId: null,
+        identityProvider: 'Windows',
+        identityProviderUserPrincipalName: 'sub456'
+      },
+      {
+        subjectId: 'azuresub123',
+        firstName: 'azure_first_1',
+        middleName: '',
+        lastName: 'azure_last_2',
+        externalIdentifier: 'external_id',
+        tenantId: 'tenant_id',
+        identityProvider: 'Azure',
+        principalType: 'user',
+        identityProviderUserPrincipalName: 'azuresub123'
       }
     ]
   };
