bdd60b46322469cda29175f49fdac0d04464efda
Kendall Miller <kendall.miller@healthcatalyst.com>
2019-01-18 10:57:46 -0700

    unneeded object reference

--- Fabric.Authorization.AccessControl/src/app/services/global/services.service.spec.ts
+++ Fabric.Authorization.AccessControl/src/app/services/global/services.service.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed, inject, async } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
 
 import { ServicesService, IService} from './services.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
