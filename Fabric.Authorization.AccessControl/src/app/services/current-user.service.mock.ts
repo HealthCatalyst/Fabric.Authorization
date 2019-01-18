e3d463bf01ca9ea1209ab58d8442687e6e3ec56c
Jason Soto <jason.soto@healthcatalyst.com>
2019-01-18 16:42:37 -0700

    member component save button messages

--- Fabric.Authorization.AccessControl/src/app/services/current-user.service.mock.ts
+++ Fabric.Authorization.AccessControl/src/app/services/current-user.service.mock.ts
@@ -1,6 +1,10 @@
 export const mockCurrentUserPermissions: string[] = [
 ];
 
+export const mockAdminUserPermissions: string[] = [
+    'dos/datamarts.manageauthorization'
+];
+
 export class CurrentUserServiceMock {
     getPermissions: jasmine.Spy = jasmine.createSpy('getPermissions');
 }
