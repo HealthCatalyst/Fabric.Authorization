3f09c70516104c8ecc8f69add67baec07972b36d
Kendall Miller <kendall.miller@healthcatalyst.com>
2019-01-18 17:55:04 -0700

    If IdentityService is installed first then IdPSS gets the requireAuthToken false from Identity Service

--- Fabric.Authorization.AccessControl/src/app/services/global/services.service.spec.ts
+++ Fabric.Authorization.AccessControl/src/app/services/global/services.service.spec.ts
@@ -15,4 +15,42 @@ describe('ServicesService', () => {
   it('should be created', inject([ServicesService], (service: ServicesService) => {
     expect(service).toBeTruthy();
   }));
+
+  it('matches correct service requireAuthToken, when services out of order',
+  inject(
+    [ServicesService], (service: ServicesService) => {
+      var strUrl = "https://testdomain.local/IdentityProviderSearchService/v1";
+      let listOfServices: IService[] = [
+        {
+          name: 'IdentityService',
+          version: 1.1,
+          url: 'https://testdomain.local/Identity',
+          requireAuthToken: false
+        },
+        {
+          name: 'IdentityProviderSearchService',
+          version: 1.2,
+          url: 'https://testdomain.local/IdentityProviderSearchService/v1',
+          requireAuthToken: true
+        },
+        {
+            name: 'AuthorizationService',
+            version: 1.3,
+            url: 'https://testdomain.local/Authorization/v1',
+            requireAuthToken: true
+        },
+        {
+            name: 'AccessControl',
+            version: 1.4,
+            url: 'https://testdomain.local/Authorization',
+            requireAuthToken: false
+        }
+    ];
+
+      service.services = listOfServices;
+      var result = service.needsAuthToken(strUrl);
+      expect(result).toBeTruthy();
+    }
+   )
+  )
 });
