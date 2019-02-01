6114d7c0ae271cb303ac7b0eacaa7f4cdcff8c83
Jason Soto <jason.soto@healthcatalyst.com>
2019-02-01 13:00:10 -0700

    Rename e2e admin spec file
    cleanup access-control page to remove most waits

--- Fabric.Authorization.AccessControl/e2e/admin-user.e2e-spec.ts
+++ Fabric.Authorization.AccessControl/e2e/admin-user.e2e-spec.ts
@@ -11,18 +11,18 @@ describe('adding a user', () => {
   it('should search and add a user with roles, and then remove the roles', () => {
     page.getMainPage();
 
-    page.searchForAndSelectUser('functional test user');
+    page.searchForAndSelectUser('functional test user', 'user');
 
     page.selectFirstRoleAndSave();
 
     // re-search existing user (with role added)
-    page.searchForAndSelectUser('functional test user');
+    page.searchForAndSelectUser('functional test user', 'user');
 
     // verify user has roles from above
     const until = protractor.ExpectedConditions;
     const rolesTable = element(by.className('hc-table'));
     const checkBoxContainer = rolesTable.element(by.css('tbody')).all(by.className('hc-checkbox-checked')).first();
-    browser.wait(until.visibilityOf(checkBoxContainer), 3000);  // fails test if not found
+    browser.wait(until.visibilityOf(checkBoxContainer), 3000, 'Selected role was not found on re-search');  // fails test if not found
 
     // remove role (reset) user
     page.selectFirstRoleAndSave();
--- Fabric.Authorization.AccessControl/e2e/pages/access-control.page.ts
+++ Fabric.Authorization.AccessControl/e2e/pages/access-control.page.ts
@@ -2,36 +2,38 @@ import { browser, protractor, element, by } from 'protractor';
 
 export class AccessControlPage {
     getMainPage() {
-        browser.waitForAngularEnabled(false)  // a redirect occurs for login?
-        .then(() => browser.get(browser.baseUrl));
+        browser.waitForAngularEnabled(false)
+            .then(() => browser.get(browser.baseUrl));
 
-        browser.driver.sleep(2000); // see if i can wait for angular above and make that work..
+        browser.driver.sleep(2000);
     }
 
-    searchForAndSelectUser(userToSearch: string) {
+    searchForAndSelectUser(searchString: string, principalType: string) {
         // click add group/user
         const until = protractor.ExpectedConditions;
         const addButton = element(by.buttonText('Add'));
-        browser.wait(until.presenceOf(addButton), 5000);  // wait until loaded...
+        browser.wait(until.presenceOf(addButton), 3000, 'Add button on main page was not found');  // wait until loaded...
         addButton.click();
         const userGroupLink = element(by.linkText('Directory Group or User'));
-        browser.wait(until.presenceOf(userGroupLink), 3000);
+        browser.wait(until.presenceOf(userGroupLink), 3000, 'Link to member page was not found');
         userGroupLink.click();
 
         const saveButton = element(by.buttonText('Save'));
         expect(saveButton.isEnabled()).toBe(false, 'save button was not disabled before selecting a user');
 
         // search user
-        element(by.className('hc-input')).sendKeys(userToSearch);
-        browser.driver.sleep(3000); // manual sleep for idpss search results..
+        element(by.className('hc-input')).sendKeys(searchString);
 
         // select result
-        const userListElement = element(by.className('member-list')).all(by.css('li')).filter((elem) => {
+        const principalListElement = element(by.className('member-list')).all(by.css('li')).filter((elem) => {
         return elem.getText().then((text) => {
-            return text.includes('(user)'); // select user list element
+            return text.includes(`(${principalType})`); // select user list element
         });
         }).first();
-        const userSelectionCheckBox = userListElement.element(by.className('hc-checkbox-overlay')); // overlay contains actual checkbox..
+        browser.wait(until.visibilityOf(principalListElement), 3000, 'IdPSS search results were not found');
+
+        // overlay contains the actual clickable checkbox
+        const userSelectionCheckBox = principalListElement.element(by.className('hc-checkbox-overlay'));
         browser.wait(until.elementToBeClickable(userSelectionCheckBox));
         userSelectionCheckBox.click();
     }
@@ -48,5 +50,12 @@ export class AccessControlPage {
 
         expect(saveButton.isEnabled()).toBe(true, 'save button was not enabled');
         saveButton.click();
+        this.waitUntilMainPageLoaded();
+    }
+
+    private waitUntilMainPageLoaded() {
+        const until = protractor.ExpectedConditions;
+        const addButton = element(by.buttonText('Add'));
+        browser.wait(until.presenceOf(addButton), 3000, 'Main page was not loaded');
     }
 }
