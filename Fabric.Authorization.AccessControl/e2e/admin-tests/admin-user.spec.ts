32cbea780a835971e0142d3df0c72fdfe0881260
Jason Soto <jason.soto@healthcatalyst.com>
2019-02-04 18:22:35 -0700

    More page stuffs

--- Fabric.Authorization.AccessControl/e2e/admin-tests/admin-user.spec.ts
+++ Fabric.Authorization.AccessControl/e2e/admin-tests/admin-user.spec.ts
@@ -2,7 +2,7 @@ import { protractor, element, browser, by } from 'protractor';
 import { MemberListPage } from '../pages/member-list.page';
 import { MemberPage } from './../pages/member.page';
 
-describe('adding a principal ad group/user', () => {
+describe('member-list page', () => {
   let mainPage: MemberListPage;
 
   beforeEach(() => {
@@ -12,7 +12,7 @@ describe('adding a principal ad group/user', () => {
   it('should search and add a user with roles, and then remove the roles', () => {
     mainPage.getMemberListPage();
 
-    const memberPage = mainPage.navigateToMemberPage();
+    const memberPage: MemberPage = mainPage.navigateToMemberPage();
     memberPage.searchForAndSelectPrincipal('functional test user', 'user');
     memberPage.selectRoleAndSave(memberPage.DataMartAdmin);
 
@@ -22,9 +22,9 @@ describe('adding a principal ad group/user', () => {
 
     // verify user has roles from above
     const until = protractor.ExpectedConditions;
-    const rolesTable = element(by.className('hc-table'));
-    const checkBoxContainer = rolesTable.element(by.css('tbody')).all(by.className('hc-checkbox-checked')).first();
-    browser.wait(until.visibilityOf(checkBoxContainer), 3000, 'Selected role was not found on re-search');  // fails test if not found
+    const checkBoxContainer = memberPage.getRoleRow(memberPage.DataMartAdmin)
+      .element(by.className('hc-checkbox-checked'));
+    browser.wait(until.visibilityOf(checkBoxContainer), 3000, 'Selected role was not selected on re-search');  // fails test if not found
 
     // remove role (reset) user
     memberPage.selectRoleAndSave(memberPage.DataMartAdmin);
@@ -41,12 +41,11 @@ describe('adding a principal ad group/user', () => {
     mainPage.navigateToMemberPage();
     memberPage.searchForAndSelectPrincipal('functional test group', 'group');
 
-
     // verify user has roles from above
     const until = protractor.ExpectedConditions;
-    const rolesTable = element(by.className('hc-table'));
-    const checkBoxContainer = rolesTable.element(by.css('tbody')).all(by.className('hc-checkbox-checked')).first();
-    browser.wait(until.visibilityOf(checkBoxContainer), 3000, 'Selected role was not found on re-search');  // fails test if not found
+    const checkBoxContainer = memberPage.getRoleRow(memberPage.PublicEntityReader)
+      .element(by.className('hc-checkbox-checked'));
+    browser.wait(until.visibilityOf(checkBoxContainer), 3000, 'Selected role was not selected on re-search');  // fails test if not found
 
     // remove role (reset) user
     memberPage.selectRoleAndSave(memberPage.PublicEntityReader);
