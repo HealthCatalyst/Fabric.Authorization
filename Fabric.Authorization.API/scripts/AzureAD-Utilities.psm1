7fcba52e3455529f6c3473e53058d69c459f38dc
Brian Smith <brian.smith@healthcatalyst.com>
2019-02-07 07:28:26 -0500

    1) put credentials prompt inside loop so user can retry on invalid credentials scenario, 2) fixed a bug related to invalid credentials during Get-AzureADGroup call

--- Fabric.Authorization.API/scripts/AzureAD-Utilities.psm1
+++ Fabric.Authorization.API/scripts/AzureAD-Utilities.psm1
@@ -9,7 +9,7 @@ try {
 Import-Module -Name DosInstallUtilities -Force
 
 # Import Fabric Install Utilities
-$fabricInstallUtilities = ".\Fabric-Install-Utilities.psm1"
+$fabricInstallUtilities = "$PSScriptRoot\Fabric-Install-Utilities.psm1"
 if (!(Test-Path $fabricInstallUtilities -PathType Leaf)) {
     Write-DosMessage -Level "Warning" -Message "Could not find fabric install utilities. Manually downloading and installing"
     Invoke-WebRequest -Uri https://raw.githubusercontent.com/HealthCatalyst/InstallScripts/master/common/Fabric-Install-Utilities.psm1 -Headers @{"Cache-Control" = "no-cache"} -OutFile $fabricInstallUtilities
