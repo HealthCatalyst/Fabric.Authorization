7e360fa5cb516279813c3625a33e9c4e980f22b7
Jason Soto <jason.soto@healthcatalyst.com>
2019-03-13 09:55:21 -0600

    Add constant file for principal 'user' and 'group'

---
+++ Fabric.Authorization.AccessControl/src/app/constants/principal-constants.ts
@@ -0,0 +1,2 @@
+export const USER = 'user';
+export const GROUP = 'group';
--- Fabric.Authorization.AccessControl/src/app/services/name-display.service.ts
+++ Fabric.Authorization.AccessControl/src/app/services/name-display.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { IFabricPrincipal } from '../models/fabricPrincipal.model';
+import { IFabricPrincipal} from '../models/fabricPrincipal.model';
+import { USER, GROUP } from '../constants/principal-constants';
 
 @Injectable({
   providedIn: 'root'
@@ -8,9 +9,9 @@ export class NameDisplayService {
   constructor() { }
 
   getPrincipalNameToDisplay(principal: IFabricPrincipal): string {
-    if (principal.principalType.toLowerCase() === 'user') {
+    if (principal.principalType.toLowerCase() === USER) {
       return principal.identityProviderUserPrincipalName || principal.subjectId;
-    }  else if (principal.principalType.toLowerCase() === 'group') {
+    }  else if (principal.principalType.toLowerCase() === GROUP) {
       if (!principal.tenantAlias) {
         return principal.subjectId;
       }
