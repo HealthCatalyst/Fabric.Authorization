5f39693158d9b3e510a73b4508d9bc55a195f486
Jason Soto <jason.soto@healthcatalyst.com>
2019-03-18 10:42:35 -0600

    Remove auth.service's services.service dependency

--- Fabric.Authorization.AccessControl/src/app/models/urlResponse.model.ts
+++ Fabric.Authorization.AccessControl/src/app/models/urlResponse.model.ts
@@ -1,10 +1,9 @@
 export class UrlResponse {
     constructor(identityUrl, accessControlUrl) {
-        this.identityUrl = identityUrl
-        this.accessControlUrl = accessControlUrl
+        this.identityUrl = identityUrl;
+        this.accessControlUrl = accessControlUrl;
     }
 
     public identityUrl: string;
     public accessControlUrl: string;
 }
-  
