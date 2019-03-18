ccbf5387396fcc70e1e4eda0634227fe0f313edb
Jason Soto <jason.soto@healthcatalyst.com>
2019-03-18 15:18:51 -0600

    Update failing test providers

--- Fabric.Authorization.AccessControl/src/app/services/global/initializer.service.spec.ts
+++ Fabric.Authorization.AccessControl/src/app/services/global/initializer.service.spec.ts
@@ -3,11 +3,22 @@ import { TestBed, inject } from '@angular/core/testing';
 import { InitializerService } from './initializer.service';
 import { IAuthService } from './auth.service';
 import { MockAuthService } from './auth.service.mock';
+import { ServicesService } from './services.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ConfigService } from './config.service';
 
 describe('InitializerService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [InitializerService, {provide: 'IAuthService', useClass: MockAuthService }]
+      imports: [HttpClientTestingModule],
+      providers: [InitializerService,
+        ServicesService,
+        ConfigService,
+        {
+          provide: 'IAuthService',
+          useClass: MockAuthService
+        }
+      ]
     });
   });
 
