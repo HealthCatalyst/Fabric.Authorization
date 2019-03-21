5d68906b446fa60322038253f058ed0b7a87f6a3
Jason Soto <jason.soto@healthcatalyst.com>
2019-03-21 11:25:42 -0600

    Services service not created if user is not yet authenticated

--- Fabric.Authorization.AccessControl/src/app/services/global/initializer.service.ts
+++ Fabric.Authorization.AccessControl/src/app/services/global/initializer.service.ts
@@ -1,12 +1,14 @@
 import { Injectable, Inject } from '@angular/core';
 import { IAuthService } from './auth.service';
+import { ServicesService } from './services.service';
 
 @Injectable()
 export class InitializerService {
 
-  constructor(@Inject('IAuthService')private authService: IAuthService) { }
+  constructor(@Inject('IAuthService')private authService: IAuthService, private servicesServce: ServicesService) { }
 
   initialize() {
-    return this.authService.initialize();
+    return this.authService.initialize()
+      .then(() => this.servicesServce.initialize());
   }
 }
