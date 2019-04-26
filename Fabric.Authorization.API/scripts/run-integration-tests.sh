732b3b3b7b146c0cc975a658833a82b347701183
Michael Vidal <michael.vidal@healthcatalyst.com>
2019-04-26 10:56:26 -0400

    Downgraded to .net core 2.1 so we can take advantage of LTS on this version.

--- Fabric.Authorization.API/scripts/run-integration-tests.sh
+++ Fabric.Authorization.API/scripts/run-integration-tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 "/C/Program Files (x86)/Microsoft Visual Studio/2017/Professional/MSBuild/15.0/Bin/MSBuild.exe" ../../Fabric.Authorization.SqlServer/Fabric.Authorization.SqlServer.sqlproj
-cp ../../Fabric.Authorization.SqlServer/bin/Debug/Fabric.Authorization.SqlServer_Create.sql ../../Fabric.Authorization.IntegrationTests/bin/Debug/netcoreapp2.2/Fabric.Authorization.SqlServer_Create.sql
+cp ../../Fabric.Authorization.SqlServer/bin/Debug/Fabric.Authorization.SqlServer_Create.sql ../../Fabric.Authorization.IntegrationTests/bin/Debug/netcoreapp2.1/Fabric.Authorization.SqlServer_Create.sql
 
 dotnet test ../../Fabric.Authorization.IntegrationTests/Fabric.Authorization.IntegrationTests.csproj
