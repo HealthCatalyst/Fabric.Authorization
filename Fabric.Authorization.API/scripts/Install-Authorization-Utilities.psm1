198e0ef5c7d3e1f2b58b338f9ead65ca5c1ec726
Kendall Miller <kendall.miller@healthcatalyst.com>
2019-11-01 12:00:48 -0600

    remove old scope

--- Fabric.Authorization.API/scripts/Install-Authorization-Utilities.psm1
+++ Fabric.Authorization.API/scripts/Install-Authorization-Utilities.psm1
@@ -987,7 +987,7 @@ function Add-AccessControlClientRegistration
         clientId = "fabric-access-control";
         clientName = "Fabric Authorization Access Control Client";
         requireConsent = "false";
-        allowedScopes = "openid", "profile", "fabric.profile", "fabric/authorization.read", "fabric/authorization.write", "fabric/authorization.internal", "fabric/identity.searchusers", "fabric/idprovider.searchusers", "fabric/authorization.dos.write";
+        allowedScopes = "openid", "profile", "fabric.profile", "fabric/authorization.read", "fabric/authorization.write", "fabric/authorization.internal", "fabric/identity.searchusers", "fabric/authorization.dos.write";
         allowOfflineAccess = $false;
         allowAccessTokensViaBrowser = $true;
         enableLocalLogin = $false;
